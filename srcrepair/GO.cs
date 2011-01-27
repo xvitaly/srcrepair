185686240d6926ac6128dc675b0b19c2c80d2470
V1TSK <vitaly@easycoding.org>
2011-01-27 08:33:47 +0000

    * Добавлен класс GO для настроек приложения и других параметров.

---
+++ srcrepair/GO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace srcrepair
+{
+    class GO
+    {
+        public static bool IsExpertMode = false;
+        public static bool EnableLogs = false;
+        public static bool HideNotInstalled = true;
+        public static bool HideNonCritMsgs = false;
+        public static string PathToClean = "";
+    }
+}
