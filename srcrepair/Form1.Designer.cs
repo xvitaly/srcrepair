fa81a8a247fd34e14d107bc3af71e3c005aeaff1
V1TSK <vitaly@easycoding.org>
2011-06-06 08:06:36 +0000

    * Удалена ненужная рамка в Редакторе конфигов.

--- srcrepair/Form1.Designer.cs
+++ srcrepair/Form1.Designer.cs
@@ -701,6 +701,7 @@
             // CE_Editor
             // 
             this.CE_Editor.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.CE_Editor.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.CE_Editor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.CE_Editor.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.CE_CVName,
