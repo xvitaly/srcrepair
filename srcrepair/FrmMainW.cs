576fa86d7e165e7c8bff708eefefeb0aee9eeb3d
V1TSK <vitaly@easycoding.org>
2013-03-25 23:21:24 +0600

    * Исправлены проблемы с регистром в пути.

--- srcrepair/FrmMainW.cs
+++ srcrepair/FrmMainW.cs
@@ -1817,7 +1817,7 @@ namespace srcrepair
                     XmlNodeList XMLNList = XMLD.GetElementsByTagName("Game");
                     for (int i = 0; i < XMLNList.Count; i++)
                     {
-                        if (XMLD.GetElementsByTagName("DirName")[i].InnerText.ToLower() == Path.GetFileName(AppSelector.Text))
+                        if (XMLD.GetElementsByTagName("DirName")[i].InnerText.ToLower() == Path.GetFileName(AppSelector.Text).ToLower())
                         {
                             GV.FullAppName = XMLD.GetElementsByTagName("DirName")[i].InnerText;
                             GV.SmallAppName = XMLD.GetElementsByTagName("SmallName")[i].InnerText;
