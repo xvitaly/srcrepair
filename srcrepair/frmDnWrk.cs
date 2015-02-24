ca303ce427be9e88c7cef3047808f7b5dbd2247f
V1TSK <vitaly@easycoding.org>
2015-02-24 19:57:31 +0500

    Реализована загрузка файлов.

--- srcrepair/frmDnWrk.cs
+++ srcrepair/frmDnWrk.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.IO;
 
 namespace srcrepair
 {
@@ -19,6 +21,32 @@ namespace srcrepair
             InitializeComponent();
             RemoteURI = R;
             LocalFile = L;
+        }
+
+        private void DownloaderCompleted(object sender, AsyncCompletedEventArgs e)
+        {
+            //
+        }
+
+        private void DownloaderProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // Отрисовываем статус в прогресс-баре...
+            try { DN_PrgBr.Value = e.ProgressPercentage; } catch (Exception Ex) { CoreLib.WriteStringToLog(Ex.Message); }
+        }
+
+        private void DownloaderStart(string URI, string FileName)
+        {
+            // Проверим существование файла и удалим...
+            if (File.Exists(FileName)) { File.Delete(FileName); }
+
+            // Начинаем асинхронную загрузку файла...
+            using (WebClient FileDownloader = new WebClient())
+            {
+                FileDownloader.Headers.Add("User-Agent", Properties.Resources.AppDnlUA);
+                FileDownloader.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloaderCompleted);
+                FileDownloader.DownloadProgressChanged += new DownloadProgressChangedEventHandler(DownloaderProgressChanged);
+                FileDownloader.DownloadFileAsync(new Uri(URI), FileName);
+            }
         }
 
         private void frmDnWrk_Load(object sender, EventArgs e)
