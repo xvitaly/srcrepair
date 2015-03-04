10b62c22ab259a8b75baad2eacec204b81832f4f
V1TSK <vitaly@easycoding.org>
2015-03-04 22:39:39 +0500

    + Добавлен новый класс HUDTlx.

---
+++ srcrepair/HUDTlx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace srcrepair
+{
+    public sealed class HUDTlx
+    {
+        public string Name;
+        public string URI;
+        public string Preview;
+        public string Site;
+        public string ArchiveDir;
+        public string InstallDir;
+        
+        public HUDTlx(string HUDName)
+        {
+            //
+        }
+    }
+}
