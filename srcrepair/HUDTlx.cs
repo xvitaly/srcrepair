a5fed5f42311f1c8c7ec9e4a0544410a3112d5c0
V1TSK <vitaly@easycoding.org>
2015-03-14 22:00:31 +0500

    = Добавлены копирайты в HUDTlx.cs.

--- srcrepair/HUDTlx.cs
+++ srcrepair/HUDTlx.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Класс системы управления HUD'ами.
+ * 
+ * Copyright 2011 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2015 EasyCoding Team.
+ * 
+ * Лицензия: GPL v3 (см. файл GPL.txt).
+ * Лицензия контента: Creative Commons 3.0 BY.
+ * 
+ * Запрещается использовать этот файл при использовании любой
+ * лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+ * 
+ * Официальный блог EasyCoding Team: http://www.easycoding.org/
+ * Официальная страница проекта: http://www.easycoding.org/projects/srcrepair
+ * 
+ * Более подробная инфорация о программе в readme.txt,
+ * о лицензии - в GPL.txt.
+*/
+using System;
 using System.Xml;
 using System.IO;
 
