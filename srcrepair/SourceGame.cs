26de20dbda78880f6b94cdd5ab963c8675f917ac
V1TSK <vitaly@easycoding.org>
2015-03-17 21:33:33 +0500

    - Удалены неиспользуемые using в новых классах.

--- srcrepair/SourceGame.cs
+++ srcrepair/SourceGame.cs
@@ -18,11 +18,8 @@
 */
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Xml;
 using System.IO;
-using System.Windows.Forms;
 
 namespace srcrepair
 {
