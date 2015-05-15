f75268d0cee20e6d6e91340b27093759b872e7fc
V1TSK <vitaly@easycoding.org>
2015-05-15 21:37:33 +0500

    + Добавлена форма нового модуля, который будет заниматься распаковкой резервных копий с выводом прогресса.

---
+++ srcrepair/FrmArchWrk.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace srcrepair
+{
+    partial class FrmArchWrk
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmArchWrk));
+            this.AR_WlcMsg = new System.Windows.Forms.Label();
+            this.AR_PrgBr = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // AR_WlcMsg
+            // 
+            resources.ApplyResources(this.AR_WlcMsg, "AR_WlcMsg");
+            this.AR_WlcMsg.Name = "AR_WlcMsg";
+            // 
+            // AR_PrgBr
+            // 
+            resources.ApplyResources(this.AR_PrgBr, "AR_PrgBr");
+            this.AR_PrgBr.Name = "AR_PrgBr";
+            // 
+            // FrmArchWrk
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ControlBox = false;
+            this.Controls.Add(this.AR_PrgBr);
+            this.Controls.Add(this.AR_WlcMsg);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmArchWrk";
+            this.ShowInTaskbar = false;
+            this.Load += new System.EventHandler(this.FrmArchWrk_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label AR_WlcMsg;
+        private System.Windows.Forms.ProgressBar AR_PrgBr;
+    }
+}
---
+++ srcrepair/FrmArchWrk.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace srcrepair
+{
+    public partial class FrmArchWrk : Form
+    {
+        public FrmArchWrk()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmArchWrk_Load(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
