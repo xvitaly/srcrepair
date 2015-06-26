19a8501458d7b32fa2eb14257cf38d368e58f290
V1TSK <vitaly@easycoding.org>
2015-06-26 16:29:23 +0500

    * Добавлена новая строка локализации PS_ProgressFinished в ресурс.

--- srcrepair/AppStrings.Designer.cs
+++ srcrepair/AppStrings.Designer.cs
@@ -1267,6 +1267,15 @@ namespace srcrepair {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Done..
+        /// </summary>
+        internal static string PS_ProgressFinished {
+            get {
+                return ResourceManager.GetString("PS_ProgressFinished", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Are you sure you want to restore factory game settings? This will remove all non-standard and old files and settings stored in Windows registry..
         /// </summary>
         internal static string PS_ResetSettingsMsg {
