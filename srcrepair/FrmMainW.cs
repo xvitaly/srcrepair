81eea5ce6c0b4d1ec1819e418053080f3b766dfd
V1TSK <vitaly@easycoding.org>
2015-09-09 22:35:43 +0500

    Добавлен обработчик нажатия на новую кнопку.

--- srcrepair/FrmMainW.cs
+++ srcrepair/FrmMainW.cs
@@ -3414,7 +3414,8 @@ namespace srcrepair
 
         private void HD_OpenDir_Click(object sender, EventArgs e)
         {
-            //
+            // Покажем файлы установленного HUD в Проводнике...
+            try { Process.Start(Properties.Settings.Default.ShBin, String.Format("{0} \"{1}\"", Properties.Settings.Default.ShParam, Path.Combine(SelGame.CustomInstallDir, SelHUD.InstallDir))); } catch (Exception Ex) { CoreLib.WriteStringToLog(Ex.Message); }
         }
     }
 }
