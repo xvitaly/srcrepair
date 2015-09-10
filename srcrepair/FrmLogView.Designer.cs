5a3bd36ff57c416bcc25a63d8a555bb2253daca7
V1TSK <vitaly@easycoding.org>
2015-09-10 21:09:25 +0500

    Добавлена новая пустая форма модуля просмотра журналов.

---
+++ srcrepair/FrmLogView.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace srcrepair
+{
+    partial class frmLogView
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogView));
+            this.SuspendLayout();
+            // 
+            // frmLogView
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.DoubleBuffered = true;
+            this.Name = "frmLogView";
+            this.Load += new System.EventHandler(this.frmLogView_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ srcrepair/FrmLogView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace srcrepair
+{
+    public partial class frmLogView : Form
+    {
+        public frmLogView()
+        {
+            InitializeComponent();
+        }
+
+        private void frmLogView_Load(object sender, EventArgs e)
+        {
+            //
+        }
+    }
+}
