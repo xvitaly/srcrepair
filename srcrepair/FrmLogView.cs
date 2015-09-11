b942f26be061c92fc070f56a6c4d53961ec829c4
V1TSK <vitaly@easycoding.org>
2015-09-11 21:38:37 +0500

    Реализовано чтение текстового файла с помещением результата в TextArea.

--- srcrepair/FrmLogView.cs
+++ srcrepair/FrmLogView.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
 
 namespace srcrepair
 {
@@ -20,7 +21,8 @@ namespace srcrepair
 
         private void frmLogView_Load(object sender, EventArgs e)
         {
-            //
+            // Считаем содержимое выбранного файла...
+            LV_LogArea.Lines = File.ReadAllLines(LogFileName);
         }
     }
 }
