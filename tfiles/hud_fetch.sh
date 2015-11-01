8b0e4fee32ef8fa581101e460b040f2b5596a966
V1TSK <vitaly@easycoding.org>
2015-11-01 21:56:57 +0500

    Добавлен стандартный для проекта заголовок копирайта в скрипт создания зеркала HUD.

--- tfiles/hud_fetch.sh
+++ tfiles/hud_fetch.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# 
+# Скрипт создания зеркала HUD.
+# 
+# Copyright 2011 - 2015 EasyCoding Team (ECTeam).
+# Copyright 2005 - 2015 EasyCoding Team.
+# 
+# Лицензия: GPL v3 (см. файл GPL.txt).
+# Лицензия контента: Creative Commons 3.0 BY.
+# 
+# Запрещается использовать этот файл при использовании любой
+# лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+# 
+# Официальный блог EasyCoding Team: http://www.easycoding.org/
+# Официальная страница проекта: http://www.easycoding.org/projects/srcrepair
+# 
+# Более подробная инфорация о программе в readme.txt,
+# о лицензии - в GPL.txt.
+# 
+
 function fetch_hud
 {
     # Выводим текст, оповещающий пользователя о начале загрузки...
