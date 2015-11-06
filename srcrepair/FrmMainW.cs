b9b380e5b8474af2de33ab79f97a119abc9f7e76
V1TSK <vitaly@easycoding.org>
2015-11-06 22:38:39 +0500

    Реализована проверка верности заполнения и твикера GCF игр.

--- srcrepair/FrmMainW.cs
+++ srcrepair/FrmMainW.cs
@@ -1734,13 +1734,19 @@ namespace srcrepair
         /// <param name="GameType">Тип игры: GCF или NCF</param>
         private bool ValidateGameSettings(bool GameType)
         {
-            return ((GT_NCF_Quality.SelectedIndex != -1) && (GT_NCF_MemPool.SelectedIndex != -1)
-                    && (GT_NCF_EffectD.SelectedIndex != -1) && (GT_NCF_ShaderE.SelectedIndex != -1)
-                    && (GT_NCF_Multicore.SelectedIndex != -1) && (GT_NCF_VSync.SelectedIndex != -1)
-                    && (GT_NCF_Filtering.SelectedIndex != -1) && (GT_NCF_AntiAlias.SelectedIndex != -1)
-                    && (GT_NCF_DispMode.SelectedIndex != -1) && (GT_NCF_Ratio.SelectedIndex != -1)
-                    && (GT_NCF_Brightness.SelectedIndex != -1) && (GT_NCF_Shadows.SelectedIndex != -1)
-                    && (GT_NCF_MBlur.SelectedIndex != -1));
+            return !GameType ? ((GT_ScreenType.SelectedIndex != -1) && (GT_ModelQuality.SelectedIndex != -1)
+                && (GT_TextureQuality.SelectedIndex != -1) && (GT_ShaderQuality.SelectedIndex != -1)
+                && (GT_WaterQuality.SelectedIndex != -1) && (GT_ShadowQuality.SelectedIndex != -1)
+                && (GT_ColorCorrectionT.SelectedIndex != -1) && (GT_AntiAliasing.SelectedIndex != -1)
+                && (GT_Filtering.SelectedIndex != -1) && (GT_VSync.SelectedIndex != -1)
+                && (GT_MotionBlur.SelectedIndex != -1) && (GT_DxMode.SelectedIndex != -1)
+                && (GT_HDR.SelectedIndex != -1)) : ((GT_NCF_Quality.SelectedIndex != -1)
+                && (GT_NCF_MemPool.SelectedIndex != -1) && (GT_NCF_EffectD.SelectedIndex != -1)
+                && (GT_NCF_ShaderE.SelectedIndex != -1) && (GT_NCF_Multicore.SelectedIndex != -1)
+                && (GT_NCF_VSync.SelectedIndex != -1) && (GT_NCF_Filtering.SelectedIndex != -1)
+                && (GT_NCF_AntiAlias.SelectedIndex != -1) && (GT_NCF_DispMode.SelectedIndex != -1)
+                && (GT_NCF_Ratio.SelectedIndex != -1) && (GT_NCF_Brightness.SelectedIndex != -1)
+                && (GT_NCF_Shadows.SelectedIndex != -1) && (GT_NCF_MBlur.SelectedIndex != -1));
         }
 
         #endregion
