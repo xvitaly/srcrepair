f03e5d3b0aaeeabb8d2ebe40d6464354e06878ce
V1TSK <vitaly@easycoding.org>
2015-12-06 22:01:36 +0500

    Добавлена локализация формы.

--- srcrepair/FrmStmClean.Designer.cs
+++ srcrepair/FrmStmClean.Designer.cs
@@ -30,19 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmStmClean));
             this.EC_GB_Basic = new System.Windows.Forms.GroupBox();
-            this.EC_HTMLCache = new System.Windows.Forms.CheckBox();
             this.EC_HTTPCache = new System.Windows.Forms.CheckBox();
+            this.EC_HTMLCache = new System.Windows.Forms.CheckBox();
             this.EC_GB_Garb = new System.Windows.Forms.GroupBox();
-            this.EC_Logs = new System.Windows.Forms.CheckBox();
-            this.EC_OldBins = new System.Windows.Forms.CheckBox();
-            this.EC_ErrDmps = new System.Windows.Forms.CheckBox();
-            this.EC_GB_ExCn = new System.Windows.Forms.GroupBox();
-            this.EC_GameIcons = new System.Windows.Forms.CheckBox();
-            this.EC_Cloud = new System.Windows.Forms.CheckBox();
-            this.EC_Stats = new System.Windows.Forms.CheckBox();
-            this.EC_Music = new System.Windows.Forms.CheckBox();
             this.EC_BuildCache = new System.Windows.Forms.CheckBox();
+            this.EC_ErrDmps = new System.Windows.Forms.CheckBox();
+            this.EC_OldBins = new System.Windows.Forms.CheckBox();
+            this.EC_Logs = new System.Windows.Forms.CheckBox();
+            this.EC_GB_ExCn = new System.Windows.Forms.GroupBox();
             this.EC_Skins = new System.Windows.Forms.CheckBox();
+            this.EC_Music = new System.Windows.Forms.CheckBox();
+            this.EC_Stats = new System.Windows.Forms.CheckBox();
+            this.EC_Cloud = new System.Windows.Forms.CheckBox();
+            this.EC_GameIcons = new System.Windows.Forms.CheckBox();
             this.EC_Execute = new System.Windows.Forms.Button();
             this.EC_GB_Trsh = new System.Windows.Forms.GroupBox();
             this.EC_Guard = new System.Windows.Forms.CheckBox();
@@ -61,17 +61,17 @@
             this.EC_GB_Basic.Name = "EC_GB_Basic";
             this.EC_GB_Basic.TabStop = false;
             // 
-            // EC_HTMLCache
-            // 
-            resources.ApplyResources(this.EC_HTMLCache, "EC_HTMLCache");
-            this.EC_HTMLCache.Name = "EC_HTMLCache";
-            this.EC_HTMLCache.UseVisualStyleBackColor = true;
-            // 
             // EC_HTTPCache
             // 
             resources.ApplyResources(this.EC_HTTPCache, "EC_HTTPCache");
             this.EC_HTTPCache.Name = "EC_HTTPCache";
             this.EC_HTTPCache.UseVisualStyleBackColor = true;
+            // 
+            // EC_HTMLCache
+            // 
+            resources.ApplyResources(this.EC_HTMLCache, "EC_HTMLCache");
+            this.EC_HTMLCache.Name = "EC_HTMLCache";
+            this.EC_HTMLCache.UseVisualStyleBackColor = true;
             // 
             // EC_GB_Garb
             // 
@@ -83,11 +83,17 @@
             this.EC_GB_Garb.Name = "EC_GB_Garb";
             this.EC_GB_Garb.TabStop = false;
             // 
-            // EC_Logs
+            // EC_BuildCache
             // 
-            resources.ApplyResources(this.EC_Logs, "EC_Logs");
-            this.EC_Logs.Name = "EC_Logs";
-            this.EC_Logs.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.EC_BuildCache, "EC_BuildCache");
+            this.EC_BuildCache.Name = "EC_BuildCache";
+            this.EC_BuildCache.UseVisualStyleBackColor = true;
+            // 
+            // EC_ErrDmps
+            // 
+            resources.ApplyResources(this.EC_ErrDmps, "EC_ErrDmps");
+            this.EC_ErrDmps.Name = "EC_ErrDmps";
+            this.EC_ErrDmps.UseVisualStyleBackColor = true;
             // 
             // EC_OldBins
             // 
@@ -95,11 +101,11 @@
             this.EC_OldBins.Name = "EC_OldBins";
             this.EC_OldBins.UseVisualStyleBackColor = true;
             // 
-            // EC_ErrDmps
+            // EC_Logs
             // 
-            resources.ApplyResources(this.EC_ErrDmps, "EC_ErrDmps");
-            this.EC_ErrDmps.Name = "EC_ErrDmps";
-            this.EC_ErrDmps.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.EC_Logs, "EC_Logs");
+            this.EC_Logs.Name = "EC_Logs";
+            this.EC_Logs.UseVisualStyleBackColor = true;
             // 
             // EC_GB_ExCn
             // 
@@ -112,23 +118,11 @@
             this.EC_GB_ExCn.Name = "EC_GB_ExCn";
             this.EC_GB_ExCn.TabStop = false;
             // 
-            // EC_GameIcons
+            // EC_Skins
             // 
-            resources.ApplyResources(this.EC_GameIcons, "EC_GameIcons");
-            this.EC_GameIcons.Name = "EC_GameIcons";
-            this.EC_GameIcons.UseVisualStyleBackColor = true;
-            // 
-            // EC_Cloud
-            // 
-            resources.ApplyResources(this.EC_Cloud, "EC_Cloud");
-            this.EC_Cloud.Name = "EC_Cloud";
-            this.EC_Cloud.UseVisualStyleBackColor = true;
-            // 
-            // EC_Stats
-            // 
-            resources.ApplyResources(this.EC_Stats, "EC_Stats");
-            this.EC_Stats.Name = "EC_Stats";
-            this.EC_Stats.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.EC_Skins, "EC_Skins");
+            this.EC_Skins.Name = "EC_Skins";
+            this.EC_Skins.UseVisualStyleBackColor = true;
             // 
             // EC_Music
             // 
@@ -136,17 +130,23 @@
             this.EC_Music.Name = "EC_Music";
             this.EC_Music.UseVisualStyleBackColor = true;
             // 
-            // EC_BuildCache
+            // EC_Stats
             // 
-            resources.ApplyResources(this.EC_BuildCache, "EC_BuildCache");
-            this.EC_BuildCache.Name = "EC_BuildCache";
-            this.EC_BuildCache.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.EC_Stats, "EC_Stats");
+            this.EC_Stats.Name = "EC_Stats";
+            this.EC_Stats.UseVisualStyleBackColor = true;
             // 
-            // EC_Skins
+            // EC_Cloud
             // 
-            resources.ApplyResources(this.EC_Skins, "EC_Skins");
-            this.EC_Skins.Name = "EC_Skins";
-            this.EC_Skins.UseVisualStyleBackColor = true;
+            resources.ApplyResources(this.EC_Cloud, "EC_Cloud");
+            this.EC_Cloud.Name = "EC_Cloud";
+            this.EC_Cloud.UseVisualStyleBackColor = true;
+            // 
+            // EC_GameIcons
+            // 
+            resources.ApplyResources(this.EC_GameIcons, "EC_GameIcons");
+            this.EC_GameIcons.Name = "EC_GameIcons";
+            this.EC_GameIcons.UseVisualStyleBackColor = true;
             // 
             // EC_Execute
             // 
