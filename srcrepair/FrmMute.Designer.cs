da9510adaa9c34f3bb0ab196df4645311a31ecc3
V1TSK <vitaly@easycoding.org>
2015-12-08 21:26:04 +0500

    Добавлена заготовка для нового модуля "Менеджер отключённых пользователей".

---
+++ srcrepair/FrmMute.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace srcrepair
+{
+    partial class FrmMute
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMute));
+            this.SuspendLayout();
+            // 
+            // FrmMute
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmMute";
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
