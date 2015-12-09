84d55d7adc7951a94490766994ac432a8014e087
V1TSK <vitaly@easycoding.org>
2015-12-09 20:11:44 +0500

    Добавлен метод ReadFileToTable(), загружающий содержимое файла в нашу форму.

--- srcrepair/FrmMute.cs
+++ srcrepair/FrmMute.cs
@@ -6,6 +6,8 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace srcrepair
 {
@@ -18,9 +20,42 @@ namespace srcrepair
             Banlist = BL;
         }
 
+        private void ReadFileToTable(string FileName)
+        {
+            // Проверим существование файла...
+            if (File.Exists(FileName))
+            {
+                // Откроем файл...
+                using (StreamReader OpenedHosts = new StreamReader(FileName, Encoding.Default))
+                {
+                    // Читаем файл до получения маркера конца файла...
+                    while (OpenedHosts.Peek() >= 0)
+                    {
+                        // Почистим строку от лишних символов...
+                        string ImpStr = CoreLib.CleanStrWx(OpenedHosts.ReadLine());
+
+                        // Представим строку как массив...
+                        List<String> Res = new List<String>(ImpStr.Split(' '));
+                        
+                        // Обойдём полученный массив в цикле...
+                        foreach (string Str in Res)
+                        {
+                            // Проверим валидность значения при помощи регулярного выражения...
+                            if (Regex.IsMatch(Str, Properties.Resources.MM_SteamIDRegex))
+                            {
+                                // Добавляем в форму...
+                                MM_Table.Rows.Add(Str);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
         private void FrmMute_Load(object sender, EventArgs e)
         {
             //
+            ReadFileToTable(Banlist);
         }
     }
 }
