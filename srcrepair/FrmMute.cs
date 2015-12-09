c65625142eb424f526b1c0837aa0458965972c8b
V1TSK <vitaly@easycoding.org>
2015-12-09 21:52:40 +0500

    Удалены неиспользуемые using.

--- srcrepair/FrmMute.cs
+++ srcrepair/FrmMute.cs
@@ -18,14 +18,10 @@
 */
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-using System.IO;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.Text;
+using System.IO;
 
 namespace srcrepair
 {
