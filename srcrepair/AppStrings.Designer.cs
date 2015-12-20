8a09710e1331d12ba24f4a4921c704bd9702a85d
V1TSK <vitaly@easycoding.org>
2015-12-20 22:38:00 +0500

    Добавлена новая строка в ресурс HD_BtnUpdateText.

--- srcrepair/AppStrings.Designer.cs
+++ srcrepair/AppStrings.Designer.cs
@@ -1105,6 +1105,15 @@ namespace srcrepair {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Update selected HUD.
+        /// </summary>
+        internal static string HD_BtnUpdateText {
+            get {
+                return ResourceManager.GetString("HD_BtnUpdateText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to HUD database outdated. Please update database from menu Help..
         /// </summary>
         internal static string HD_DbOutdated {
