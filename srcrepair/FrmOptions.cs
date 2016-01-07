462c45099e8cf971eb398552a418375b92e00158
V1TSK <vitaly@easycoding.org>
2016-01-07 22:28:21 +0500

    Из FrmOptions.cs удалены неиспользуемые using.

--- srcrepair/FrmOptions.cs
+++ srcrepair/FrmOptions.cs
@@ -17,12 +17,7 @@
  * о лицензии - в GPL.txt.
 */
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using System.Text.RegularExpressions;
 
