013d6116b4bd4d76eaf0c7678c8e28d1b032ec2e
V1TSK <vitaly@easycoding.org>
2016-01-07 22:26:34 +0500

    Из FrmFPGen.cs удалены неиспользуемые using.

--- srcrepair/FrmFPGen.cs
+++ srcrepair/FrmFPGen.cs
@@ -17,12 +17,6 @@
  * о лицензии - в GPL.txt.
 */
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 
 namespace srcrepair
