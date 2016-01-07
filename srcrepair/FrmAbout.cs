636279a4ae2cac2c4f0cfa7ee82140e472e62658
V1TSK <vitaly@easycoding.org>
2016-01-07 22:24:51 +0500

    Из FrmAbout.cs удалены неиспользуемые using.

--- srcrepair/FrmAbout.cs
+++ srcrepair/FrmAbout.cs
@@ -17,10 +17,6 @@
  * о лицензии - в GPL.txt.
 */
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Linq;
 using System.Reflection;
 using System.Windows.Forms;
 
