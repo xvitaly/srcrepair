8fcea941200f9bb8bd71843ae885c7033bf012bd
V1TSK <vitaly@easycoding.org>
2016-01-07 22:25:42 +0500

    Из FrmBRep.cs удалены неиспользуемые using.

--- srcrepair/FrmBRep.cs
+++ srcrepair/FrmBRep.cs
@@ -17,15 +17,11 @@
  * о лицензии - в GPL.txt.
 */
 using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using System.Net;
-using System.Web;
 using System.IO;
 using System.Reflection;
 using System.Drawing.Drawing2D;
