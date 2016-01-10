c4e21d5e83eb9c726c3a1aa358501087d7397514
V1TSK <vitaly@easycoding.org>
2016-01-10 19:18:06 +0500

    Инкрементируем билд. Начата разработка ветки 24.x.

--- srcrepair/Properties/AssemblyInfo.cs
+++ srcrepair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("23.0.0.3128")]
-[assembly: AssemblyFileVersion("23.0.0.3128")]
+[assembly: AssemblyVersion("23.0.0.3295")]
+[assembly: AssemblyFileVersion("23.0.0.3295")]
