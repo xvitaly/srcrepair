0b50eede0da49f11cc1d34040e1347c6fec4605f
V1TSK <vitaly@easycoding.org>
2016-01-14 21:18:29 +0500

    Добавлен ysHUD.

--- tfiles/hud_fetch.sh
+++ tfiles/hud_fetch.sh
@@ -92,3 +92,4 @@ fetch_hud http://huds.tf/yayahud/img/main.png https://github.com/Yttrium-tYcLief
 fetch_hud http://huds.tf/smesihud/img/main.png https://github.com/Smesi/SmesiHud/archive/master.zip smesihud
 fetch_hud http://huds.tf/rainhud/img/main.png https://github.com/rainoflight/rainhud/archive/master.zip rainhud
 fetch_hud http://huds.tf/solarhud/img/main.png https://github.com/Stochast1c/solarhud/archive/master.zip solarhud
+fetch_hud http://huds.tf/yshud/img/main.png https://github.com/Yo5hi/ysHUD/archive/master.zip yshud
