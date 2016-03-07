1d78253b9a20f0ea0f0cd4cdcbd8f0a63cfd2287
V1TSK <vitaly@easycoding.org>
2016-03-07 20:50:41 +0500

    Реализован конструктор класса HUDManager.

--- srcrepair/HUDManager.cs
+++ srcrepair/HUDManager.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Xml;
 using System.Text;
 
 namespace srcrepair
@@ -8,11 +10,26 @@ namespace srcrepair
     public sealed class HUDManager
     {
         private List<HUDTlx> HUDsAvailable;
-        private HUDTlx SelectedHUD;
+        public HUDTlx SelectedHUD;
 
-        public HUDManager()
+        public HUDManager(string HUDDbFile, string AppHUDDir)
         {
-            //
+            // Инициализируем наш список...
+            HUDsAvailable = new List<HUDTlx>();
+
+            // Получаем полный список доступных HUD для данной игры. Открываем поток...
+            using (FileStream XMLFS = new FileStream(HUDDbFile, FileMode.Open, FileAccess.Read))
+            {
+                // Загружаем XML из потока...
+                XmlDocument XMLD = new XmlDocument();
+                XMLD.Load(XMLFS);
+
+                // Разбираем XML файл и обходим его в цикле...
+                for (int i = 0; i < XMLD.GetElementsByTagName("HUD").Count; i++)
+                {
+                    try { HUDsAvailable.Add(new HUDTlx(AppHUDDir, XMLD.GetElementsByTagName("Name")[i].InnerText, XMLD.GetElementsByTagName("Game")[i].InnerText, XMLD.GetElementsByTagName("URI")[i].InnerText, XMLD.GetElementsByTagName("UpURI")[i].InnerText, XMLD.GetElementsByTagName("IsUpdated")[i].InnerText == "1", XMLD.GetElementsByTagName("Preview")[i].InnerText, XMLD.GetElementsByTagName("Site")[i].InnerText, XMLD.GetElementsByTagName("ArchiveDir")[i].InnerText, XMLD.GetElementsByTagName("InstallDir")[i].InnerText, Path.Combine(AppHUDDir, Path.ChangeExtension(Path.GetFileName(XMLD.GetElementsByTagName("Name")[i].InnerText), ".zip")))); } catch (Exception Ex) { CoreLib.WriteStringToLog(Ex.Message); }
+                }
+            }
         }
     }
 }
