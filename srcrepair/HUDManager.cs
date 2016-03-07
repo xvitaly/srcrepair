ff753d7467ec3a6feeef18e2696e387d2aa06360
V1TSK <vitaly@easycoding.org>
2016-03-07 20:05:36 +0500

    Добавлена заготовка нового класса HUDManager.

---
+++ srcrepair/HUDManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace srcrepair
+{
+    public sealed class HUDManager
+    {
+        private List<HUDTlx> HUDsAvailable;
+        private HUDTlx SelectedHUD;
+
+        public HUDManager()
+        {
+            //
+        }
+    }
+}
