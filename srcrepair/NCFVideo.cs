c1cacdafd9a5dd9645fcbc2094f5a8509ab6cf90
V1TSK <vitaly@easycoding.org>
2016-05-15 20:12:25 +0300

    Добавлена документация для полей класса NCFVideo. Добавлены новые поля.

--- srcrepair/NCFVideo.cs
+++ srcrepair/NCFVideo.cs
@@ -25,20 +25,94 @@ namespace srcrepair
     /// </summary>
     public class NCFVideo : VideoSettings
     {
+        /// <summary>
+        /// Хранит путь к актуальному файлу с графическими настройками игры.
+        /// </summary>
         private string VideoFile;
+
+        /// <summary>
+        /// Хранит путь к файлу со стандартными настройками для текущего ПК.
+        /// </summary>
         private string DefaultsFile;
+
+        /// <summary>
+        /// Хранит настройки соотношения сторон NCF-игры на движке Source 1 (setting.aspectratiomode).
+        /// </summary>
         private int ScreenRatio;
+
+        /// <summary>
+        /// Хранит настройки контрастности и цветовой гаммы NCF-игры на движке Source 1 (setting.mat_monitorgamma).
+        /// </summary>
         private int Brightness;
+
+        /// <summary>
+        /// Хранит настройки качества теней NCF-игры на движке Source 1 (setting.csm_quality_level).
+        /// </summary>
         private int ShadowQuality;
+
+        /// <summary>
+        /// Хранит настройки размытия движений NCF-игры на движке Source 1 (setting.mat_motion_blur_enabled).
+        /// </summary>
         private int MotionBlur;
+
+        /// <summary>
+        /// Хранит настройки графического режима NCF-игры на движке Source 1 (setting.fullscreen).
+        /// </summary>
         private int DisplayMode;
+
+        /// <summary>
+        /// Хранит настройки графического режима NCF-игры на движке Source 1 (setting.nowindowborder).
+        /// </summary>
+        private int DisplayBorderless;
+
+        /// <summary>
+        /// Хранит настройки полноэкранного сглаживания NCF-игры на движке Source 1 (setting.mat_antialias).
+        /// </summary>
         private int AntiAliasing;
-        private int Filtering;
+
+        /// <summary>
+        /// Хранит значение глубины полноэкранного сглаживания NCF-игры на движке Source 1 (setting.mat_aaquality).
+        /// </summary>
+        private int AntiAliasQuality;
+
+        /// <summary>
+        /// Хранит настройки качества фильтрации текстур NCF-игры на движке Source 1 (setting.mat_forceaniso).
+        /// </summary>
+        private int FilteringMode;
+
+        /// <summary>
+        /// Хранит настройки вертикальной синхронизации NCF-игры на движке Source 1 (setting.mat_vsync).
+        /// </summary>
         private int VSync;
+
+        /// <summary>
+        /// Хранит настройки качества вертикальной синхронизации NCF-игры на движке Source 1 (setting.mat_triplebuffered).
+        /// </summary>
+        private int VSyncMode;
+
+        /// <summary>
+        /// Хранит настройки многоядерного рендеринга NCF-игры на движке Source 1 (setting.mat_queue_mode).
+        /// </summary>
         private int MCRendering;
+
+        /// <summary>
+        /// Хранит настройки качества шейдерных эффектов NCF-игры на движке Source 1 (setting.gpu_level).
+        /// </summary>
         private int ShaderEffects;
+
+        /// <summary>
+        /// Хранит настройки качества обычных эффектов NCF-игры на движке Source 1 (setting.cpu_level).
+        /// </summary>
         private int EffectDetails;
+
+        /// <summary>
+        /// Хранит настройки выделенного игре пула памяти NCF-игры на движке Source 1 (setting.mem_level).
+        /// </summary>
         private int MemoryPoolType;
+
+        /// <summary>
+        /// Хранит настройки качества детализации моделей и текстур NCF-игры на движке Source 1 (setting.gpu_mem_level).
+        /// </summary>
         private int TextureModelQuality;
     }
 }
