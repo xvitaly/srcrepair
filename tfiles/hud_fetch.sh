0ff0bfa236d82e36737b3d60419271742324f3b9
V1TSK <vitaly@easycoding.org>
2016-05-22 22:12:10 +0500

    Обновлён год в копирайте скрипта создания зеркал HUD.

--- tfiles/hud_fetch.sh
+++ tfiles/hud_fetch.sh
@@ -3,8 +3,8 @@
 # 
 # Скрипт создания зеркала HUD.
 # 
-# Copyright 2011 - 2015 EasyCoding Team (ECTeam).
-# Copyright 2005 - 2015 EasyCoding Team.
+# Copyright 2011 - 2016 EasyCoding Team (ECTeam).
+# Copyright 2005 - 2016 EasyCoding Team.
 # 
 # Лицензия: GPL v3 (см. файл GPL.txt).
 # Лицензия контента: Creative Commons 3.0 BY.
