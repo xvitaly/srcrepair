a66a9f045804938f99d60639d11d54941ba1d705
V1TSK <vitaly@easycoding.org>
2016-06-04 19:21:54 +0300

    Реализован конструктор класса ConfigManager.

--- srcrepair/ConfigManager.cs
+++ srcrepair/ConfigManager.cs
@@ -18,8 +18,10 @@
 */
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace srcrepair
 {
@@ -41,9 +43,24 @@ namespace srcrepair
         /// <summary>
         /// Конструктор класса. Читает базу данных в формате XML и заполняет нашу структуру.
         /// </summary>
-        public ConfigManager()
+        public ConfigManager(string CfgDbFile, string LangPrefix)
         {
-            //
+            // Инициализируем список...
+            Configs = new List<CFGTlx>();
+
+            // Получаем полный список доступных конфигов. Открываем поток...
+            using (FileStream XMLFS = new FileStream(CfgDbFile, FileMode.Open, FileAccess.Read))
+            {
+                // Загружаем XML из потока...
+                XmlDocument XMLD = new XmlDocument();
+                XMLD.Load(XMLFS);
+
+                // Разбираем XML файл и обходим его в цикле...
+                for (int i = 0; i < XMLD.GetElementsByTagName("Config").Count; i++)
+                {
+                    try { Configs.Add(new CFGTlx(XMLD.GetElementsByTagName("Name")[i].InnerText, XMLD.GetElementsByTagName("FileName")[i].InnerText, XMLD.GetElementsByTagName(LangPrefix)[i].InnerText, XMLD.GetElementsByTagName("SupportedGames")[i].InnerText.Split(','))); } catch (Exception Ex) { CoreLib.WriteStringToLog(Ex.Message); }
+                }
+            }
         }
     }
 }
