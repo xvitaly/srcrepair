ddf78c345d809cab2622c730d7cc379e3c84fe5d
Vitaly Zaitsev <vitaly@easycoding.org>
2016-07-15 22:48:49 +0500

    Все устаревшие HUD были удалены из базы данных.

--- tfiles/hud_fetch.sh
+++ tfiles/hud_fetch.sh
@@ -45,35 +45,22 @@ function fetch_hud
 
 # From GitHub...
 fetch_hud https://github.com/Sevin7/7HUD/archive/master.zip 7HUD
-fetch_hud https://github.com/piklestf2/pikles-hud/archive/master.zip pikles-hud
 fetch_hud https://github.com/fblue/broeselhud_blue/archive/master.zip broeselhud_blue
-fetch_hud https://github.com/basbanaan/bastHUD/archive/master.zip basthud
-fetch_hud https://github.com/sirgrey/SirHUD/archive/master.zip sirhud
-fetch_hud https://github.com/SnowshoeIceboot/TF2HudPlus/archive/master.zip tf2hudplus
 fetch_hud https://github.com/TheStaticVoid/VoidHUD2.0/archive/master.zip voidhud
 fetch_hud https://github.com/n0kk/ahud/archive/master.zip ahud
-fetch_hud https://github.com/Slayer89/slayhud/archive/master.zip calmlikeabomb
-fetch_hud https://github.com/basbanaan/EJP-HUD/archive/master.zip ejphud
 fetch_hud https://github.com/flatlinee/flatHUD-/archive/master.zip flathud
 fetch_hud https://github.com/bw-/bwhud/archive/master.zip bwhud
 fetch_hud https://github.com/WhiskerBiscuit/budhud/archive/master.zip budhud
 fetch_hud https://github.com/Eniere/idhud/archive/master.zip idhud
 fetch_hud https://github.com/omnibombulator/noto/archive/master.zip noto
-fetch_hud https://github.com/ItsMorgus/MorgHUD/archive/master.zip morghud
 fetch_hud https://github.com/Jotunn/KBNHud/archive/master.zip kbnhud
-fetch_hud https://github.com/Intellectualbadass/medHUD/archive/master.zip medhud
 fetch_hud https://github.com/raysfire/rayshud/archive/master.zip rayshud
 fetch_hud https://github.com/MedicodiBiscotti/biscottiHUD/archive/master.zip biscottihud
-fetch_hud https://github.com/mattr0d/flamehud/archive/master.zip flamehud
 fetch_hud https://github.com/TheKins/frankenhud/archive/master.zip frankenhud
 fetch_hud https://github.com/Xeletron/Isaac-Hud/archive/master.zip isaachud
-fetch_hud https://github.com/kermit-tf/JayHUD/archive/master.zip jayhud
 fetch_hud https://github.com/mannterface/Mannterface/archive/master.zip mannterface
 fetch_hud https://github.com/Yttrium-tYcLief/yayahud/archive/master.zip yayahud
-fetch_hud https://github.com/Smesi/SmesiHud/archive/master.zip smesihud
-fetch_hud https://github.com/rainoflight/rainhud/archive/master.zip rainhud
 fetch_hud https://github.com/Stochast1c/solarhud/archive/master.zip solarhud
-fetch_hud https://github.com/Yo5hi/ysHUD/archive/master.zip yshud
 fetch_hud https://github.com/TheStaticVoid/boredHUD/archive/master.zip boredhud
 fetch_hud https://github.com/hoXyy/GMang_HUD/archive/master.zip gmanghud
 fetch_hud https://github.com/CriticalFlaw/FlawHUD/archive/master.zip flawhud
@@ -87,10 +74,5 @@ fetch_hud https://github.com/flatlinee/omphudedit/archive/master.zip omphud
 fetch_hud https://github.com/rsbear/wavesui/archive/master.zip wavesuihud
 
 # Other sources...
-fetch_hud https://gitgud.io/JediThug/JediHUD/repository/archive.zip jedihud
 fetch_hud http://files.gamebanana.com/guis/eve_hud_v401.zip evehud
-fetch_hud http://files.gamebanana.com/guis/revhud-jan17.zip revhud
 fetch_hud http://huds.tf/forum/xthreads_attach.php/151_1468069037_c96274f0/2e863cebac5b6ff6c2c39aa2b42fdf99/DoggyHudV2.zip doggyhud
-
-# Need to be repacked...
-fetch_hud https://github.com/JackStanley/TF2slimHUD/archive/Main.zip slimhud
