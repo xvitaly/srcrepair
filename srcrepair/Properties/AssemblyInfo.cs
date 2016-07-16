a0a7e10ac7c3e02bf15dc38a6d329078e92b0002
V1TSK <vitaly@easycoding.org>
2016-07-16 16:25:45 +0300

    Инкрементирован билд.

--- srcrepair/Properties/AssemblyInfo.cs
+++ srcrepair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("25.0.0.4159")]
-[assembly: AssemblyFileVersion("25.0.0.4159")]
+[assembly: AssemblyVersion("25.0.0.4161")]
+[assembly: AssemblyFileVersion("25.0.0.4161")]
