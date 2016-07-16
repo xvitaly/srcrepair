525d9a25078a8eba916b02b5ff8c3dd1a7eacee6
V1TSK <vitaly@easycoding.org>
2016-07-16 17:07:09 +0300

    Повторно инкрементирован билд с переключением на stable версию.

--- srcrepair/Properties/AssemblyInfo.cs
+++ srcrepair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("25.0.0.4161")]
-[assembly: AssemblyFileVersion("25.0.0.4161")]
+[assembly: AssemblyVersion("25.0.0.4162")]
+[assembly: AssemblyFileVersion("25.0.0.4162")]
