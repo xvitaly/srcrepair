dc7187102166a28c8f2aeb3a2c76df76a96d02e6
Vitaly Zaitsev <vitaly@easycoding.org>
2016-07-26 21:49:27 +0500

    Добавлен стандартный для проекта заголовок и явно указана кодировка файла скрипта.

--- tfiles/hudman.py
+++ tfiles/hudman.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python3
+# coding=utf-8
+
+#
+# Скрипт создания и обновления зеркала HUD.
+#
+# Copyright 2011 - 2016 EasyCoding Team (ECTeam).
+# Copyright 2005 - 2016 EasyCoding Team.
+#
+# Лицензия: GPL v3 (см. файл GPL.txt).
+# Лицензия контента: Creative Commons 3.0 BY.
+#
+# Запрещается использовать этот файл при использовании любой
+# лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+#
+# Официальный блог EasyCoding Team: http://www.easycoding.org/
+# Официальная страница проекта: http://www.easycoding.org/projects/srcrepair
+#
+# Более подробная инфорация о программе в readme.txt,
+# о лицензии - в GPL.txt.
+#
 
