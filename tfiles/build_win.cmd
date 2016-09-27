10791438db70394076eb58ada7544d8c5d46bf63
V1TSK <vitaly@easycoding.org>
2016-09-27 19:59:15 +0300

    Добавлен скрипт для автоматической сборки релизной версии SRC Repair посредством командной строки.

--- tfiles/build_win.cmd
+++ tfiles/build_win.cmd
@@ -1,2 +1,13 @@
 @echo off
 
+echo Starting build process using MSBUILD...
+"%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" ..\srcrepair.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.0
+
+echo Changing directory to built version...
+cd "..\srcrepair\bin\Release"
+
+echo Signing binaries...
+"%ProgramFiles%\Git\bin\bash.exe" -i gpg-sign.sh
+
+echo Compiling Installer...
+"%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" srcrepair.iss
