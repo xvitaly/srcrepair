f7405e60ef87102536ea2c7bc9af3f845f61b518
Vitaly Zaitsev <vitaly@easycoding.org>
2016-10-14 17:56:51 +0300

    Добавлен стандартный для проекта копирайт в SteamManager.cs.

--- srcrepair/SteamManager.cs
+++ srcrepair/SteamManager.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Класс для взаимодействия с клиентом Steam.
+ * 
+ * Copyright 2011 - 2016 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2016 EasyCoding Team.
+ * 
+ * Лицензия: GPL v3 (см. файл GPL.txt).
+ * Лицензия контента: Creative Commons 3.0 BY.
+ * 
+ * Запрещается использовать этот файл при использовании любой
+ * лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+ * 
+ * Официальный блог EasyCoding Team: http://www.easycoding.org/
+ * Официальная страница проекта: http://www.easycoding.org/projects/srcrepair
+ * 
+ * Более подробная инфорация о программе в readme.txt,
+ * о лицензии - в GPL.txt.
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using Gameloop.Vdf;
