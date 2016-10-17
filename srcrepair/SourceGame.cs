cb8e34fb9282bb839e9a1f29e236568bd2767f87
Vitaly Zaitsev <vitaly@easycoding.org>
2016-10-17 20:11:38 +0300

    Временно отключён механизм загрузки параметров запуска игры.

--- srcrepair/SourceGame.cs
+++ srcrepair/SourceGame.cs
@@ -281,7 +281,7 @@ namespace srcrepair
                 if (IsUsingVideoFile) { UpdateVideoFilesList(); }
                 UpdateBanlistFilesList();
                 CloudConfigs = GetCloudConfigs();
-                try { LaunchOptions = SteamManager.GetLaunchOptions(SteamPath, GameInternalID); } catch { /* Do nothing. */ }
+                //try { LaunchOptions = SteamManager.GetLaunchOptions(SteamPath, GameInternalID); } catch { /* Do nothing. */ }
             }
         }
     }
