67b4b046494cf294897d9294cf6a20efe08f1fcd
Vitaly Zaitsev <vitaly@easycoding.org>
2016-11-04 20:22:28 +0300

    Добавлены элементы управления на форму модуля выбора SteamID.

--- srcrepair/FrmStmSelector.Designer.cs
+++ srcrepair/FrmStmSelector.Designer.cs
@@ -29,12 +29,44 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmStmSelector));
+            this.SD_WMsg = new System.Windows.Forms.Label();
+            this.SD_SteamSel = new System.Windows.Forms.ComboBox();
+            this.ST_OK = new System.Windows.Forms.Button();
+            this.ST_Cancel = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // SD_WMsg
+            // 
+            resources.ApplyResources(this.SD_WMsg, "SD_WMsg");
+            this.SD_WMsg.Name = "SD_WMsg";
+            // 
+            // SD_SteamSel
+            // 
+            this.SD_SteamSel.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.SD_SteamSel.FormattingEnabled = true;
+            resources.ApplyResources(this.SD_SteamSel, "SD_SteamSel");
+            this.SD_SteamSel.Name = "SD_SteamSel";
+            // 
+            // ST_OK
+            // 
+            resources.ApplyResources(this.ST_OK, "ST_OK");
+            this.ST_OK.Name = "ST_OK";
+            this.ST_OK.UseVisualStyleBackColor = true;
+            // 
+            // ST_Cancel
+            // 
+            resources.ApplyResources(this.ST_Cancel, "ST_Cancel");
+            this.ST_Cancel.Name = "ST_Cancel";
+            this.ST_Cancel.UseVisualStyleBackColor = true;
             // 
             // FrmStmSelector
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ST_Cancel);
+            this.Controls.Add(this.ST_OK);
+            this.Controls.Add(this.SD_SteamSel);
+            this.Controls.Add(this.SD_WMsg);
             this.Name = "FrmStmSelector";
             this.ShowInTaskbar = false;
             this.ResumeLayout(false);
@@ -42,5 +74,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Label SD_WMsg;
+        private System.Windows.Forms.ComboBox SD_SteamSel;
+        private System.Windows.Forms.Button ST_OK;
+        private System.Windows.Forms.Button ST_Cancel;
     }
 }
