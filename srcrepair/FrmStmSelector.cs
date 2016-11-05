cc2ca02b98d0b32ddbf4262c24372455c5e65acc
Vitaly Zaitsev <vitaly@easycoding.org>
2016-11-05 19:39:53 +0300

    Добавлена документация для методов и свойств класса FrmStmSelector.

--- srcrepair/FrmStmSelector.cs
+++ srcrepair/FrmStmSelector.cs
@@ -12,21 +12,38 @@ namespace srcrepair
 {
     public partial class FrmStmSelector : Form
     {
+        /// <summary>
+        /// Хранит и возвращает SteamID.
+        /// </summary>
         public string SteamID { get; private set; }
+
+        /// <summary>
+        /// Хранит список полученных SteamID.
+        /// </summary>
         private List<String> SteamIDs;
 
+        /// <summary>
+        /// Конструктор класса формы модуля выбора SteamID.
+        /// </summary>
+        /// <param name="S">Список SteamID для выбора</param>
         public FrmStmSelector(List<String> S)
         {
             InitializeComponent();
             SteamIDs = S;
         }
 
+        /// <summary>
+        /// Метод, срабатывающий при возникновении события "загрузка формы".
+        /// </summary>
         private void FrmStmSelector_Load(object sender, EventArgs e)
         {
             // Указываем откуда следует брать список со SteamID...
             SD_IDSel.DataSource = SteamIDs;
         }
 
+        /// <summary>
+        /// Метод, срабатывающий при нажатии на кнопку "OK".
+        /// </summary>
         private void ST_OK_Click(object sender, EventArgs e)
         {
             // Возвращаем результат...
@@ -39,6 +56,9 @@ namespace srcrepair
             Close();
         }
 
+        /// <summary>
+        /// Метод, срабатывающий при нажатии на кнопку "Отмена".
+        /// </summary>
         private void ST_Cancel_Click(object sender, EventArgs e)
         {
             // Возвращаем результат формы "отменено"...
