8ee867ce58b7d15b9561c72e5c9bddf0d3e96e7d
Vitaly Zaitsev <vitaly@easycoding.org>
2016-12-17 20:51:49 +0300

    Добавлены новые открытые свойства класса NCFSettings.

--- srcrepair/NCFSettings.cs
+++ srcrepair/NCFSettings.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace srcrepair
 {
-    class NCFSettings
+    public class NCFSettings
     {
+        public string ScreenWidth { get; private set; }
+        public string ScreenHeight { get; private set; }
+        public string ScreenRatio { get; private set; }
+        public string Brightness { get; private set; }
+        public string ShadowQuality { get; private set; }
+        public string MotionBlur { get; private set; }
+        public string DisplayMode { get; private set; }
+        public string DisplayBorderless { get; private set; }
+        public string AntiAliasing { get; private set; }
+        public string AntiAliasQuality { get; private set; }
+        public string FilteringMode { get; private set; }
+        public string VSync { get; private set; }
+        public string VSyncMode { get; private set; }
+        public string MCRendering { get; private set; }
+        public string ShaderEffects { get; private set; }
+        public string EffectDetails { get; private set; }
+        public string MemoryPoolType { get; private set; }
+        public string TextureModelQuality { get; private set; }
+
+        public NCFSettings(string ID)
+        {
+            //
+        }
     }
 }
