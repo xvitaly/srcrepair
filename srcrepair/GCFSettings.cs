a680503e84e07203c7698ef54185c0055dcfa866
Vitaly Zaitsev <vitaly@easycoding.org>
2016-12-19 19:18:46 +0300

    Добавлен новый закрытый метод SetSettingsV1() класса GCFSettings.

--- srcrepair/GCFSettings.cs
+++ srcrepair/GCFSettings.cs
@@ -27,9 +27,32 @@ namespace srcrepair
         public string DirectXMode { get; private set; }
         public string HDRMode { get; private set; }
 
+        private void SetSettingsV1()
+        {
+            ScreenWidth = "ScreenWidth";
+            ScreenHeight = "ScreenHeight";
+            DisplayMode = "ScreenWindowed";
+            ModelDetail = "r_rootlod";
+            TextureDetail = "mat_picmip";
+            ShaderDetail = "mat_reducefillrate";
+            WaterDetail = "r_waterforceexpensive";
+            WaterReflections = "r_waterforcereflectentities";
+            ShadowDetail = "r_shadowrendertotexture";
+            ColorCorrection = "mat_colorcorrection";
+            AntiAliasing = "mat_antialias";
+            AntiAliasQuality = "mat_aaquality";
+            FilteringMode = "mat_forceaniso";
+            FilteringTrilinear = "mat_trilinear";
+            VSync = "mat_vsync";
+            MotionBlur = "MotionBlur";
+            DirectXMode = "DXLevel_V1";
+            HDRMode = "mat_hdr_level";
+        }
+
         public GCFSettings()
         {
-            //
+            // Заполняем значения...
+            SetSettingsV1();
         }
     }
 }
