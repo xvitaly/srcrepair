a3e47c9f3de0ceea89d0a3b2d61dc4ca83bf32aa
Vitaly Zaitsev <vitaly@easycoding.org>
2016-12-19 19:45:26 +0300

    Добавлен стандартный для проекта копирайт в GCFSettings.cs. Удалены неиспользуемые using.

--- srcrepair/GCFSettings.cs
+++ srcrepair/GCFSettings.cs
@@ -1,8 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Служебный класс работы с настройками графики GCF игр.
+ * 
+ * Copyright 2011 - 2017 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2017 EasyCoding Team.
+ * 
+ * Лицензия: GPL v3 (см. файл GPL.txt).
+ * Лицензия контента: Creative Commons 3.0 BY.
+ * 
+ * Запрещается использовать этот файл при использовании любой
+ * лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+ * 
+ * Официальный блог EasyCoding Team: http://www.easycoding.org/
+ * Официальная страница проекта: http://www.easycoding.org/projects/srcrepair
+ * 
+ * Более подробная инфорация о программе в readme.txt,
+ * о лицензии - в GPL.txt.
+*/
 
 namespace srcrepair
 {
