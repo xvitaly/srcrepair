c978d31559c9a7ab7cc41144c33cdbafbfcb5304
Vitaly Zaitsev <vitaly@easycoding.org>
2017-05-12 17:55:34 +0300

    Исправлен путь к установленному MSBuild пакета Visual Studio после конвертирования до версии 2017.

--- tfiles/build_win.cmd
+++ tfiles/build_win.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Starting build process using MSBUILD...
-"%ProgramFiles(x86)%\MSBuild\14.0\bin\msbuild.exe" ..\srcrepair.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\srcrepair.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
 
 echo Changing directory to built version...
 cd "..\srcrepair\bin\Release"
