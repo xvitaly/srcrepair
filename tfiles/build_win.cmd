2c301fc4b54ee9834b9e86b5d907a0fdb242d2a4
Vitaly Zaitsev <vitaly@easycoding.org>
2017-05-12 17:57:13 +0300

    Разрешена параллельная сборка проекта посредством MSBuild.

--- tfiles/build_win.cmd
+++ tfiles/build_win.cmd
@@ -1,7 +1,7 @@
 @echo off
 
 echo Starting build process using MSBUILD...
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\srcrepair.sln /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
+"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\srcrepair.sln /m /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
 
 echo Changing directory to built version...
 cd "..\srcrepair\bin\Release"
