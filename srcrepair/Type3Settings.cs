f6ef1390184f89b85c1c6878d9f18e60912ad8a2
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-02 19:06:15 +0300

    Удалены неиспользуемые using из Type3Settings.cs.

--- srcrepair/Type3Settings.cs
+++ srcrepair/Type3Settings.cs
@@ -16,11 +16,6 @@
  * Более подробная инфорация о программе в readme.txt,
  * о лицензии - в GPL.txt.
 */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace srcrepair
 {
