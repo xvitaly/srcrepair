64f9dde2ad0634cd3173937bed6199999fae1b52
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-02 20:23:50 +0300

    Добавлена третья порция новых свойств класса Type3Settings.

--- srcrepair/Type3Settings.cs
+++ srcrepair/Type3Settings.cs
@@ -117,6 +117,86 @@ namespace srcrepair
         public string SimpleLight { get; private set; }
 
         /// <summary>
+        /// Хранит имя настройки SSAO для освещения.
+        /// </summary>
+        public string SSAOLights { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки глобальной карты теней.
+        /// </summary>
+        public string GlobalShadowMode { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки полноэкранного сглаживания.
+        /// </summary>
+        public string AntiAliasing { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки глубины полноэкранного сглаживания.
+        /// </summary>
+        public string AntiAliasQuality { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки портретной анимации.
+        /// </summary>
+        public string PortraitAnimation { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки Specular Shading.
+        /// </summary>
+        public string SpecularShading { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки Specular Bloom Shading.
+        /// </summary>
+        public string SpecularBloomShading { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки рендеринга второстепенных предметов на карте.
+        /// </summary>
+        public string AmbientCreatures { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки стриминга текстур.
+        /// </summary>
+        public string TextureStreaming { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки карт нормалей.
+        /// </summary>
+        public string NormalMaps { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки качества детализации таблицы результатов.
+        /// </summary>
+        public string DashboardQuality { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки качества шейдерных эффектов.
+        /// </summary>
+        public string ShaderQuality { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки рекомендованного разрешения по вертикали.
+        /// </summary>
+        public string RecommendedHeight { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки области рендеринга.
+        /// </summary>
+        public string ViewportScale { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки контрастности и цветовой гаммы.
+        /// </summary>
+        public string Brightness { get; private set; }
+
+        /// <summary>
+        /// Хранит имя настройки активации расширенных опций видео.
+        /// </summary>
+        public string UseAdvanced { get; private set; }
+
+        /// <summary>
         /// Базовый конструктор класса.
         /// </summary>
         public Type3Settings()
