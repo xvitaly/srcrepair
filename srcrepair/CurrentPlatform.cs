04f342e55ea5ae7c2ca5a70645ab9318f38f5002
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-11 19:15:14 +0300

    Добавлен стандартный для проекта копирайт в CurrentPlatform.cs.

--- srcrepair/CurrentPlatform.cs
+++ srcrepair/CurrentPlatform.cs
@@ -1,4 +1,22 @@
-﻿using System;
+﻿/*
+ * Класс для определения запущенной платформы и работы с ней.
+ * 
+ * Copyright 2011 - 2017 EasyCoding Team (ECTeam).
+ * Copyright 2005 - 2017 EasyCoding Team.
+ * 
+ * Лицензия: GPL v3 (см. файл GPL.txt).
+ * Лицензия контента: Creative Commons 3.0 BY.
+ * 
+ * Запрещается использовать этот файл при использовании любой
+ * лицензии, отличной от GNU GPL версии 3 и с ней совместимой.
+ * 
+ * Официальный блог EasyCoding Team: https://www.easycoding.org/
+ * Официальная страница проекта: https://www.easycoding.org/projects/srcrepair
+ * 
+ * Более подробная инфорация о программе в readme.txt,
+ * о лицензии - в GPL.txt.
+*/
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
