370f1e0c3b3976f11c1dee9866c7f27d45b17d84
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-11 19:07:14 +0300

    Добавлено новое открытое статическое свойство-геттер RunningOS класса CurrentPlatform.

--- srcrepair/CurrentPlatform.cs
+++ srcrepair/CurrentPlatform.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace srcrepair
 {
     public class CurrentPlatform
     {
+        public static int RunningOS
+        {
+            get
+            {
+                switch (Environment.OSVersion.Platform)
+                {
+                    case PlatformID.Unix:
+                        return (Directory.Exists("/Applications") & Directory.Exists("/System") & Directory.Exists("/Users") & Directory.Exists("/Volumes")) ? 1 : 2;
+                    case PlatformID.MacOSX:
+                        return 1;
+                    default: return 0;
+                }
+            }
+        }
     }
 }
