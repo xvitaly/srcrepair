9e877f2100ab24f908691395ab8f324750daed27
Vitaly Zaitsev <vitaly@easycoding.org>
2017-06-19 18:18:58 +0300

    GPG ключ для подписи сборки вынесен в отдельную константу.

--- tfiles/build_win.cmd
+++ tfiles/build_win.cmd
@@ -1,4 +1,5 @@
 @echo off
+set GPGKEY=D45AB90A
 
 echo Starting build process using MSBUILD...
 "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\msbuild.exe" ..\srcrepair.sln /m /t:Build /p:Configuration=Release /p:TargetFramework=v4.6.1
@@ -7,9 +8,9 @@ echo Changing directory to built version...
 cd "..\srcrepair\bin\Release"
 
 echo Signing binaries...
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A srcrepair.exe
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A DotNetZip.dll
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key D45AB90A ru/srcrepair.resources.dll
+"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% srcrepair.exe
+"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% DotNetZip.dll
+"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% ru/srcrepair.resources.dll
 
 echo Compiling Installer...
 "%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" srcrepair.iss
