bf977680ca00ff5234714f3b1b6db54c87d65164
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-08 03:14:20 +0300

    Добавлена новая строка локализации HD_HashError.

--- srcrepair/AppStrings.Designer.cs
+++ srcrepair/AppStrings.Designer.cs
@@ -979,6 +979,15 @@ namespace srcrepair {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Failed to check hash sum of downloaded file. Maybe it was damaged. Please try again later!.
+        /// </summary>
+        internal static string HD_HashError {
+            get {
+                return ResourceManager.GetString("HD_HashError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Installing HUD... Wait..
         /// </summary>
         internal static string HD_InstallBtnProgress {
