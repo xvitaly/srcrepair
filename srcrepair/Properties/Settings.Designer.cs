d672182f09a50d7ab7d776310198c12f9a1ef256
Vitaly Zaitsev <vitaly@easycoding.org>
2017-08-20 19:57:32 +0300

    Добавлено новое свойство LaunchCounter, которое будет использоваться для учёта количества запусков приложения.

--- srcrepair/Properties/Settings.Designer.cs
+++ srcrepair/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace srcrepair.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.1.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -245,6 +245,18 @@ namespace srcrepair.Properties {
             }
             set {
                 this["AutoUpdateCheck"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0")]
+        public long LaunchCounter {
+            get {
+                return ((long)(this["LaunchCounter"]));
+            }
+            set {
+                this["LaunchCounter"] = value;
             }
         }
     }
