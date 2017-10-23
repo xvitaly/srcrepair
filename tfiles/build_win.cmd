525df79edd4cc2d8cfb4ac4dc4e5604f0946a58a
Vitaly Zaitsev <vitaly@easycoding.org>
2017-10-23 18:58:35 +0300

    Обновлены пути в скрипте сборки для совместимости с новейшей версией GnuPG for Windows.

--- tfiles/build_win.cmd
+++ tfiles/build_win.cmd
@@ -28,9 +28,9 @@ echo Changing directory to built version...
 cd "..\srcrepair\bin\Release"
 
 echo Signing binaries...
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% srcrepair.exe
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% DotNetZip.dll
-"%ProgramFiles(x86)%\GNU\GnuPG\gpg2.exe" --sign --detach-sign --default-key %GPGKEY% ru/srcrepair.resources.dll
+"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% srcrepair.exe
+"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% DotNetZip.dll
+"%ProgramFiles(x86)%\GnuPG\bin\gpg.exe" --sign --detach-sign --default-key %GPGKEY% ru/srcrepair.resources.dll
 
 echo Compiling Installer...
 "%ProgramFiles(x86)%\Inno Setup 5\ISCC.exe" srcrepair.iss
