fa13888a5ee4765a6b233dd30ec68d57cdebc145
Vitaly Zaitsev <vitaly@easycoding.org>
2017-11-15 17:46:40 +0100

    Реализован конструктор класса GameManager.

--- srcrepair/GameManager.cs
+++ srcrepair/GameManager.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace srcrepair
 {
@@ -16,8 +18,38 @@ namespace srcrepair
             SelectedGame = SourceGames.Find(Item => String.Equals(Item.FullAppName, GameName, StringComparison.CurrentCultureIgnoreCase));
         }
 
-        public GameManager()
+        public GameManager(string FullSteamPath, string SteamAppsFolderName, string FullAppPath)
         {
+            // При использовании нового метода поиска установленных игр, считаем их из конфига Steam...
+            List<String> GameDirs = SteamManager.FormatInstallDirs(FullSteamPath, SteamAppsFolderName);
+
+            // Создаём поток с XML-файлом...
+            using (FileStream XMLFS = new FileStream(Path.Combine(FullAppPath, Properties.Resources.GameListFile), FileMode.Open, FileAccess.Read))
+            {
+                // Создаём объект документа XML...
+                XmlDocument XMLD = new XmlDocument();
+
+                // Загружаем поток в объект XML документа...
+                XMLD.Load(XMLFS);
+
+                // Обходим полученный список в цикле...
+                XmlNodeList XMLNode = XMLD.GetElementsByTagName("Game");
+                for (int i = 0; i < XMLNode.Count; i++)
+                {
+                    try
+                    {
+                        if (XMLD.GetElementsByTagName("Enabled")[i].InnerText == "1" || !Properties.Settings.Default.HideUnsupportedGames)
+                        {
+                            SourceGame SG = new SourceGame(XMLNode[i].Attributes["Name"].Value, XMLD.GetElementsByTagName("DirName")[i].InnerText, XMLD.GetElementsByTagName("SmallName")[i].InnerText, XMLD.GetElementsByTagName("Executable")[i].InnerText, XMLD.GetElementsByTagName("SID")[i].InnerText, XMLD.GetElementsByTagName("SVer")[i].InnerText, XMLD.GetElementsByTagName("VFDir")[i].InnerText, App.Platform.OS == CurrentPlatform.OSType.Windows ? XMLD.GetElementsByTagName("HasVF")[i].InnerText == "1" : true, XMLD.GetElementsByTagName("UserDir")[i].InnerText == "1", XMLD.GetElementsByTagName("HUDsAvail")[i].InnerText == "1", App.FullAppPath, App.AppUserDir, App.FullSteamPath, App.Platform.SteamAppsFolderName, GameDirs);
+                            if (SG.IsInstalled)
+                            {
+                                SourceGames.Add(SG);
+                            }
+                        }
+                    }
+                    catch (Exception Ex) { CoreLib.WriteStringToLog(Ex.Message); }
+                }
+            }
         }
     }
 }
