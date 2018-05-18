1ee35c18faec62cf2c9dda7e05c879ed2964083d
Vitaly Zaitsev <vitaly@easycoding.org>
2018-05-18 16:34:40 +0100

    Incremented version and build number.

--- srcrepair/Properties/AssemblyInfo.cs
+++ srcrepair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("32.0.1.6170")]
-[assembly: AssemblyFileVersion("32.0.1.6170")]
+[assembly: AssemblyVersion("32.0.2.6190")]
+[assembly: AssemblyFileVersion("32.0.2.6190")]
--- tfiles/inno/srcrepair.iss
+++ tfiles/inno/srcrepair.iss
@@ -41,7 +41,7 @@ DefaultGroupName=SRC Repair
 AllowNoIcons=yes
 LicenseFile=..\COPYING
 #ifdef _RELEASE
-OutputBaseFilename=srcrepair_321_final
+OutputBaseFilename=srcrepair_322_final
 #else
 OutputBaseFilename=snapshot_{#CI_COMMIT}
 #endif
