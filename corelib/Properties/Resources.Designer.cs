5dc6c95fb49909cd36c601006e05fda09264565b
Vitaly Zaitsev <vitaly@easycoding.org>
2019-05-22 13:44:44 +0200

    Changed default namespace of corelib.

--- corelib/Properties/Resources.Designer.cs
+++ corelib/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace corelib.Properties {
+namespace srcrepair.core.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace corelib.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("corelib.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("srcrepair.core.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
