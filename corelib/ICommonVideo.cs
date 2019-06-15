13ba001104a3456b5e70f865f44ca26d838f201b
Vitaly Zaitsev <vitaly@easycoding.org>
2019-06-15 16:51:44 +0200

    Added more useful documentation for ICommonVideo interface.

--- corelib/ICommonVideo.cs
+++ corelib/ICommonVideo.cs
@@ -20,15 +20,45 @@
 */
 namespace srcrepair.core
 {
+    /// <summary>
+    /// Interface with common properties and methods for
+    /// all types of games.
+    /// </summary>
     interface ICommonVideo
     {
+        /// <summary>
+        /// Gets and sets anti-aliasing video setting.
+        /// </summary>
         int AntiAliasing { get; set; }
+
+        /// <summary>
+        /// Gets and sets motion blur video setting.
+        /// </summary>
         int MotionBlur { get; set; }
+
+        /// <summary>
+        /// Gets and sets shadow quality video setting.
+        /// </summary>
         int ShadowQuality { get; set; }
+
+        /// <summary>
+        /// Gets and sets screen width video setting.
+        /// </summary>
         int ScreenWidth { get; set; }
+
+        /// <summary>
+        /// Gets and sets screen height video setting.
+        /// </summary>
         int ScreenHeight { get; set; }
+
+        /// <summary>
+        /// Gets and sets vertical synchronization video setting.
+        /// </summary>
         int VSync { get; set; }
 
+        /// <summary>
+        /// Saves video settings to file or registry.
+        /// </summary>
         void WriteSettings();
     }
 }
