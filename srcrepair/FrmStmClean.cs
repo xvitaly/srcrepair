fa02c415ff3f1bcbd5759b338bc4cceca0325bc5
Vitaly Zaitsev <vitaly@easycoding.org>
2019-08-15 11:53:12 +0200

    Refactored FrmStmClean class. Updated documentation.
    Closes #264.

--- srcrepair/FrmStmClean.cs
+++ srcrepair/FrmStmClean.cs
@@ -22,42 +22,41 @@ using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
 using System.IO;
-using srcrepair.core;
 
 namespace srcrepair.gui
 {
     /// <summary>
-    /// Класс формы модуля очистки кэшей Steam.
+    /// Class of Steam garbage cleaner window.
     /// </summary>
     public partial class FrmStmClean : Form
     {
         /// <summary>
-        /// Хранит путь к каталогу установки клиента Steam.
+        /// Gets or sets full path to Steam client directory.
         /// </summary>
         private string SteamPath { get; set; }
 
         /// <summary>
-        /// Хранит путь к каталогу хранения резервных копий.
+        /// Gets or sets full path to directory for saving backups.
         /// </summary>
         private string BackUpDir { get; set; }
 
         /// <summary>
-        /// Хранит платформо-зависимое название каталога SteamApps.
+        /// Gets or sets platform-dependent SteamApps directory name.
         /// </summary>
         private string SteamAppsDirName { get; set; }
 
         /// <summary>
-        /// Хранит платформо-зависимое имя процесса Steam.
+        /// Get or sets platform-dependent Steam process name.
         /// </summary>
         private string SteamProcName { get; set; }
 
         /// <summary>
-        /// Конструктор класса формы модуля очистки кэшей Steam.
+        /// FrmStmClean class constructor.
         /// </summary>
-        /// <param name="SP">Каталог установки Steam</param>
-        /// <param name="BD">Каталог хранения резервных копий</param>
-        /// <param name="BD">Платформо-зависимое название каталога SteamApps</param>
-        /// <param name="BD">Платформо-зависимое имя процесса Steam</param>
+        /// <param name="SP">Full path to Steam client directory.</param>
+        /// <param name="BD">Full path to directory for saving backups.</param>
+        /// <param name="SA">Platform-dependent SteamApps directory name.</param>
+        /// <param name="PN">Platform-dependent Steam process name.</param>
         public FrmStmClean(string SP, string BD, string SA, string PN)
         {
             InitializeComponent();
@@ -68,14 +67,16 @@ namespace srcrepair.gui
         }
 
         /// <summary>
-        /// Метод, срабатывающий при нажатии на кнопку запуска очистки.
+        /// "Execute cleanup" button click event handler.
         /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
         private void EC_Execute_Click(object sender, EventArgs e)
         {
-            // Создаём список файлов и каталогов для очистки...
+            // Creatin an empty list for storing candidates...
             List<String> CleanDirs = new List<string>();
 
-            // Очистим HTML-кэш внутреннего браузера Steam...
+            // HTML cache of internal browser (both client and overlay)...
             if (EC_HTMLCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "config", "htmlcache", "*.*"));
@@ -83,97 +84,105 @@ namespace srcrepair.gui
                 CleanDirs.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Steam", "htmlcache", "*.*"));
             }
 
+            // HTTP cache...
             if (EC_HTTPCache.Checked)
             {
-                // Очистим HTTP-кэш...
                 CleanDirs.Add(Path.Combine(SteamPath, "appcache", "httpcache", "*.*"));
             }
 
+            // Depot cache...
             if (EC_DepotCache.Checked)
             {
-                // Очистим Depot-кэш...
                 CleanDirs.Add(Path.Combine(SteamPath, "depotcache", "*.*"));
             }
 
+            // Downloaded shaders cache...
             if (EC_ShaderCache.Checked)
             {
-                // Очистим кэш шейдеров клиента Steam...
                 CleanDirs.Add(Path.Combine(SteamPath, "shadercache", "*.*"));
             }
 
+            // Client logs...
             if (EC_Logs.Checked)
             {
-                // Очистим логи клиента Steam...
                 CleanDirs.Add(Path.Combine(SteamPath, "logs", "*.*"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.log*"));
             }
 
+            // Old binaries...
             if (EC_OldBins.Checked)
             {
-                // Удаляем старые бинарные файлы клиента Steam...
                 CleanDirs.Add(Path.Combine(SteamPath, "*.old"));
                 CleanDirs.Add(Path.Combine(SteamPath, "bin", "*.old"));
             }
 
+            // Crash dumps...
             if (EC_ErrDmps.Checked)
             {
-                // Очистим краш-дампы...
                 CleanDirs.Add(Path.Combine(SteamPath, "dumps", "*.*"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.dmp"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.mdmp"));
             }
 
+            // Games download and build caches...
             if (EC_BuildCache.Checked)
             {
-                // Очистим кэш сборки обновлений игр с новой системой контента...
                 CleanDirs.Add(Path.Combine(SteamPath, SteamAppsDirName, "downloading"));
                 CleanDirs.Add(Path.Combine(SteamPath, SteamAppsDirName, "temp"));
             }
 
+            // Cached icons...
             if (EC_GameIcons.Checked)
             {
-                // Очистим кэшированные значки игр...
                 CleanDirs.Add(Path.Combine(SteamPath, "steam", "games", "*.*"));
             }
 
+            // Local Cloud contents...
             if (EC_Cloud.Checked)
             {
-                // Очистим локальное зеркало Cloud...
                 CleanDirs.Add(Path.Combine(SteamPath, "userdata", "*.*"));
             }
 
+            // Local offline game statistics...
             if (EC_Stats.Checked)
             {
-                // Очистим кэшированную статистику игр...
                 CleanDirs.Add(Path.Combine(SteamPath, "appcache", "stats", "*.*"));
             }
 
+            // Music database...
             if (EC_Music.Checked)
             {
-                // Очистим базу данных функции Steam Music...
                 CleanDirs.Add(Path.Combine(SteamPath, "music", "_database", "*.*"));
             }
 
+            // Custom skins...
             if (EC_Skins.Checked)
             {
-                // Очистим установленные скины Steam...
                 CleanDirs.Add(Path.Combine(SteamPath, "skins", "*.*"));
             }
 
+            // Old updater packages...
             if (EC_Updater.Checked)
             {
-                // Очистим кэш обновлений Steam...
                 CleanDirs.Add(Path.Combine(SteamPath, "package", "*.*"));
             }
 
+            // Guard supercookies and auth files...
             if (EC_Guard.Checked)
             {
-                // Удаляем кэш Steam Guard...
                 CleanDirs.Add(Path.Combine(SteamPath, "ssfn*"));
             }
 
-            // Запустим очистку...
-            if (CleanDirs.Count > 0) { GuiHelpers.FormShowCleanup(CleanDirs, Text, AppStrings.PS_CleanupSuccess, BackUpDir, SteamProcName); } else { MessageBox.Show(AppStrings.AC_NoItemsSelected, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning); }
+            // Executing cleanup sequence...
+            if (CleanDirs.Count > 0)
+            {
+                GuiHelpers.FormShowCleanup(CleanDirs, Text, AppStrings.PS_CleanupSuccess, BackUpDir, SteamProcName);
+                Close();
+            }
+            else
+            {
+                MessageBox.Show(AppStrings.AC_NoItemsSelected, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
         }
     }
 }
