80059f37ba53c9f837657a408054e792901cc81a
Vitaly Zaitsev <vitaly@easycoding.org>
2019-08-20 13:50:27 +0200

    Added documentation for ReportTarget class.

--- corelib/ReportTarget.cs
+++ corelib/ReportTarget.cs
@@ -21,10 +21,27 @@
 
 namespace srcrepair.core
 {
+    /// <summary>
+    /// Class for working with a single target, used for
+    /// generating reports.
+    /// </summary>
     public sealed class ReportTarget
     {
+        /// <summary>
+        /// Gets or sets full path to application executable.
+        /// </summary>
         public string Program { get; private set; }
+
+        /// <summary>
+        /// Gets or sets additional command-line arguments.
+        /// </summary>
         public string Parameters { get; private set; }
+
+        /// <summary>
+        /// ReportTarget class constructor.
+        /// </summary>
+        /// <param name="RtProgram">Full path to application executable.</param>
+        /// <param name="RtParameters">Additional command-line arguments.</param>
         public ReportTarget(string RtProgram, string RtParameters)
         {
             Program = RtProgram;
