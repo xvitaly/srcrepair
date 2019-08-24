e8933c9c5eb78fbac8499070ee5e08bfc98820bc
Vitaly Zaitsev <vitaly@easycoding.org>
2019-08-24 13:25:00 +0200

    Refactored method BwGen_DoWork().
    Closes #236.

--- srcrepair/FrmRepBuilder.cs
+++ srcrepair/FrmRepBuilder.cs
@@ -19,6 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Text;
@@ -78,12 +79,108 @@ namespace srcrepair.gui
         /// </summary>
         private SourceGame SelectedGame { get; set; }
 
+        private ReportManager RepMan { get; set; }
+
         /// <summary>
         /// Метод, срабатывающий при возникновении события "загрузка формы".
         /// </summary>
         private void FrmRepBuilder_Load(object sender, EventArgs e)
         {
-            // Событие создания формы...
+            RepMan = new ReportManager(AppUserDir);
+        }
+
+        private void RepCreateDirectories()
+        {
+            if (!Directory.Exists(RepMan.ReportsDirectory))
+            {
+                Directory.CreateDirectory(RepMan.ReportsDirectory);
+            }
+
+            if (!Directory.Exists(RepMan.TempDirectory))
+            {
+                Directory.CreateDirectory(RepMan.TempDirectory);
+            }
+        }
+
+        private void RepCreateReport()
+        {
+            using (ZipFile ZBkUp = new ZipFile(RepMan.ReportArchiveName, Encoding.UTF8))
+            {
+                // Adding generic report files...
+                foreach (ReportTarget RepTarget in RepMan.ReportTargets)
+                {
+                    try
+                    {
+                        ProcessManager.StartProcessAndWait(RepTarget.Program, String.Format(RepTarget.Parameters, RepTarget.OutputFileName));
+                        if (File.Exists(RepTarget.OutputFileName))
+                        {
+                            ZBkUp.AddFile(RepTarget.OutputFileName, RepTarget.ArchiveDirectoryName);
+                        }
+                    }
+                    catch (Exception Ex)
+                    {
+                        Logger.Warn(Ex);
+                    }
+                }
+
+                // Adding game configs...
+                if (Directory.Exists(SelectedGame.FullCfgPath))
+                {
+                    ZBkUp.AddDirectory(SelectedGame.FullCfgPath, "configs");
+                }
+
+                // Adding video file...
+                if (SelectedGame.IsUsingVideoFile)
+                {
+                    string GameVideo = SelectedGame.GetActualVideoFile();
+                    if (File.Exists(GameVideo))
+                    {
+                        ZBkUp.AddFile(GameVideo, "video");
+                    }
+                }
+
+                // Adding Steam crash dumps...
+                if (Directory.Exists(Path.Combine(FullSteamPath, "dumps")))
+                {
+                    ZBkUp.AddDirectory(Path.Combine(FullSteamPath, "dumps"), "dumps");
+                }
+
+                // Adding Steam logs...
+                if (Directory.Exists(Path.Combine(FullSteamPath, "logs")))
+                {
+                    ZBkUp.AddDirectory(Path.Combine(FullSteamPath, "logs"), "logs");
+                }
+
+                // Adding Hosts file contents...
+                if (File.Exists(FileManager.GetHostsFileFullPath(CurrentPlatform.OSType.Windows)))
+                {
+                    ZBkUp.AddFile(FileManager.GetHostsFileFullPath(CurrentPlatform.OSType.Windows), "hosts");
+                }
+
+                // Adding application debug log...
+                if (Directory.Exists(CurrentApp.LogDirectoryPath))
+                {
+                    ZBkUp.AddDirectory(CurrentApp.LogDirectoryPath, "debug");
+                }
+
+                // Saving Zip file to disk...
+                ZBkUp.Save();
+            }
+        }
+
+        private void RepCleanupDirectories()
+        {
+            try
+            {
+                if (Directory.Exists(RepMan.TempDirectory))
+                {
+                    Directory.Delete(RepMan.TempDirectory, true);
+                }
+            }
+            catch (Exception Ex)
+            {
+                Logger.Warn(Ex);
+            }
         }
 
         /// <summary>
@@ -92,133 +189,14 @@ namespace srcrepair.gui
         /// </summary>
         private void BwGen_DoWork(object sender, DoWorkEventArgs e)
         {
-            // Сгенерируем путь для каталога с рапортами...
-            string RepDir = Path.Combine(AppUserDir, "reports");
+            // Creating directories if does not exists...
+            RepCreateDirectories();
 
-            // Проверим чтобы каталог для рапортов существовал...
-            if (!Directory.Exists(RepDir))
-            {
-                // Не существует, поэтому создадим...
-                Directory.CreateDirectory(RepDir);
-            }
-            
-            // Генерируем пути к каталогам для вреенных файлов и создаём их...
-            string TempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
-            string CrDt = FileManager.DateTime2Unix(DateTime.Now);
-            if (!Directory.Exists(TempDir)) { Directory.CreateDirectory(TempDir); }
-            
-            // Генерируем именя файлов с полными путями...
-            string RepName = String.Format("report_{0}.{1}", CrDt, "txt");
-            string ArchName = Path.Combine(RepDir, Path.ChangeExtension(RepName, ".zip"));
-            string HostsFile = FileManager.GetHostsFileFullPath(CurrentPlatform.OSType.Windows);
-            string FNameRep = Path.Combine(TempDir, RepName);
-            string FNameFPSCfg = Path.Combine(TempDir, String.Format("fpscfg_{0}.zip", CrDt));
-            string FNamePing = Path.Combine(TempDir, String.Format("ping_{0}.log", CrDt));
-            string FNameTrace = Path.Combine(TempDir, String.Format("traceroute_{0}.log", CrDt));
-            string FNameIpConfig = Path.Combine(TempDir, String.Format("ipconfig_{0}.log", CrDt));
-            string FNameRouting = Path.Combine(TempDir, String.Format("routing_{0}.log", CrDt));
-            string FNameNetStat = Path.Combine(TempDir, String.Format("netstat_{0}.log", CrDt));
-            string FNameDxDiag = Path.Combine(TempDir, String.Format("dxdiag_{0}.log", CrDt));
-            string FNameUsers = Path.Combine(TempDir, String.Format("users_{0}.log", CrDt));
+            // Creating Zip archive...
+            RepCreateReport();
 
-            // Начинаем сборку отчёта...
-            try
-            {
-                // Генерируем основной отчёт...
-                try { ProcessManager.StartProcessAndWait("msinfo32.exe", String.Format("/report \"{0}\"", FNameRep)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                
-                // Если пользователь вдруг отменил его создание, больше ничего не делаем...
-                if (File.Exists(FNameRep))
-                {
-                    // Запускаем последовательность...
-                    try { ProcessManager.StartProcessAndWait("dxdiag.exe", String.Format("/t {0}", FNameDxDiag)); } catch (Exception Ex) { Logger.Warn(Ex); } /* DxDiag неадекватно реагирует на кавычки в пути. */
-                    try { if (SelectedGame.FPSConfigs.Count > 0) { FileManager.CompressFiles(SelectedGame.FPSConfigs, FNameFPSCfg); } } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C ping steampowered.com > \"{0}\"", FNamePing)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C tracert steampowered.com > \"{0}\"", FNameTrace)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C ipconfig /all > \"{0}\"", FNameIpConfig)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C netstat -a > \"{0}\"", FNameNetStat)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C route print > \"{0}\"", FNameRouting)); } catch (Exception Ex) { Logger.Warn(Ex); }
-                    try { ProcessManager.StartProcessAndWait("cmd.exe", String.Format("/C net user > \"{0}\"", FNameUsers)); } catch (Exception Ex) { Logger.Warn(Ex); }
-
-                    try
-                    {
-                        // Создаём Zip-архив...
-                        using (ZipFile ZBkUp = new ZipFile(ArchName, Encoding.UTF8))
-                        {
-                            // Добавляем в архив созданный рапорт...
-                            if (File.Exists(FNameRep)) { ZBkUp.AddFile(FNameRep, "report"); }
-
-                            // Добавляем в архив все конфиги выбранной игры...
-                            if (Directory.Exists(SelectedGame.FullCfgPath)) { ZBkUp.AddDirectory(SelectedGame.FullCfgPath, "configs"); }
-                            if (SelectedGame.IsUsingVideoFile) { string GameVideo = SelectedGame.GetActualVideoFile(); if (File.Exists(GameVideo)) { ZBkUp.AddFile(GameVideo, "video"); } }
-
-                            // Добавляем в архив все краш-дампы и логи Steam...
-                            if (Directory.Exists(Path.Combine(FullSteamPath, "dumps"))) { ZBkUp.AddDirectory(Path.Combine(FullSteamPath, "dumps"), "dumps"); }
-                            if (Directory.Exists(Path.Combine(FullSteamPath, "logs"))) { ZBkUp.AddDirectory(Path.Combine(FullSteamPath, "logs"), "logs"); }
-
-                            // Добавляем содержимое файла Hosts...
-                            if (File.Exists(HostsFile)) { ZBkUp.AddFile(HostsFile, "hosts"); }
-
-                            // Добавляем в архив отчёты утилит ping, трассировки и т.д.
-                            if (File.Exists(FNamePing)) { ZBkUp.AddFile(FNamePing, "system"); }
-                            if (File.Exists(FNameTrace)) { ZBkUp.AddFile(FNameTrace, "system"); }
-                            if (File.Exists(FNameIpConfig)) { ZBkUp.AddFile(FNameIpConfig, "system"); }
-                            if (File.Exists(FNameRouting)) { ZBkUp.AddFile(FNameRouting, "system"); }
-                            if (File.Exists(FNameNetStat)) { ZBkUp.AddFile(FNameNetStat, "system"); }
-                            if (File.Exists(FNameDxDiag)) { ZBkUp.AddFile(FNameDxDiag, "system"); }
-                            if (File.Exists(FNameUsers)) { ZBkUp.AddFile(FNameUsers, "system"); }
-                            if (File.Exists(FNameFPSCfg)) { ZBkUp.AddFile(FNameFPSCfg, "fps"); }
-
-                            // Добавляем в архив журнал программы...
-                            try { if (Directory.Exists(CurrentApp.LogDirectoryPath)) { ZBkUp.AddDirectory(CurrentApp.LogDirectoryPath, "debug"); } } catch (Exception Ex) { Logger.Warn(Ex); }
-
-                            // Сохраняем архив...
-                            ZBkUp.Save();
-                        }
-
-                        // Выводим сообщение об успешном создании отчёта...
-                        MessageBox.Show(String.Format(AppStrings.RPB_ComprGen, Path.GetFileName(ArchName)), PluginName, MessageBoxButtons.OK, MessageBoxIcon.Information);
-
-                        // Открываем каталог с отчётами в оболочке и выделяем созданный файл...
-                        if (File.Exists(ArchName))
-                        {
-                            try
-                            {
-                                ProcessManager.OpenExplorer(ArchName, CurrentPlatform.OSType.Windows);
-                            }
-                            catch (Exception Ex)
-                            {
-                                Logger.Warn(Ex, DebugStrings.AppDbgExRepFm);
-                            }
-                        }
-                    }
-                    catch (Exception Ex)
-                    {
-                        MessageBox.Show(AppStrings.PS_ArchFailed, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                        Logger.Error(Ex, DebugStrings.AppDbgExRepPack);
-                    }
-                }
-
-                // Выполняем очистку...
-                try
-                {
-                    // Удаляем не сжатый отчёт...
-                    if (File.Exists(FNameRep)) { File.Delete(FNameRep); }
-                    
-                    // Удаляем временный каталог...
-                    if (Directory.Exists(TempDir)) { Directory.Delete(TempDir, true); }
-                }
-                catch (Exception Ex)
-                {
-                    Logger.Warn(Ex);
-                }
-            }
-            catch (Exception Ex)
-            {
-                // Произошло исключение...
-                MessageBox.Show(AppStrings.RPB_GenException, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                Logger.Error(Ex, DebugStrings.AppDbgExRepGen);
-            }
+            // Removing temporary files...
+            RepCleanupDirectories();
         }
 
         /// <summary>
@@ -227,15 +205,36 @@ namespace srcrepair.gui
         /// </summary>
         private void BwGen_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
         {
-            // Меняем текст на кнопке...
-            GenerateNow.Text = AppStrings.RPB_CloseCpt;
-            
-            // Снова активируем ранее отключённые контролы...
-            GenerateNow.Enabled = true;
-            ControlBox = true;
+            if (e.Error == null)
+            {
+                GenerateNow.Text = AppStrings.RPB_CloseCpt;
+                GenerateNow.Enabled = true;
+                ControlBox = true;
+                IsCompleted = true;
 
-            // Переключаем свойство...
-            IsCompleted = true;
+                // Открываем каталог с отчётами в оболочке и выделяем созданный файл...
+                if (File.Exists(RepMan.ReportArchiveName))
+                {
+                    try
+                    {
+                        MessageBox.Show(String.Format(AppStrings.RPB_ComprGen, Path.GetFileName(RepMan.ReportArchiveName)), PluginName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        ProcessManager.OpenExplorer(RepMan.ReportArchiveName, CurrentPlatform.OSType.Windows);
+                    }
+                    catch (Exception Ex)
+                    {
+                        Logger.Warn(Ex, DebugStrings.AppDbgExRepFm);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show(AppStrings.PS_ArchFailed, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                MessageBox.Show(AppStrings.RPB_GenException, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Logger.Error(e.Error, DebugStrings.AppDbgExRepPack);
+            }
         }
 
         /// <summary>
