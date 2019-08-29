edb746a8d9e8d919c7d47801b41ac3c733994981
Vitaly Zaitsev <vitaly@easycoding.org>
2019-08-29 16:36:57 +0200

    Added new empty class ReturnCodes.

---
+++ corelib/ReturnCodes.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * This file is a part of SRC Repair project. For more information
+ * visit official site: https://www.easycoding.org/projects/srcrepair
+ * 
+ * Copyright (c) 2011 - 2019 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2019 EasyCoding Team.
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srcrepair.core
+{
+    public static class ReturnCodes
+    {
+    }
+}
