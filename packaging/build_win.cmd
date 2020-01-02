633bdb5b9559940873e0d7713d63e4a3f4b0a03d
Vitaly Zaitsev <vitaly@easycoding.org>
2020-01-02 12:37:25 +0100

    Updated year in GPL preambles and copyrights.
    Closes #318.

--- packaging/build_win.cmd
+++ packaging/build_win.cmd
@@ -3,8 +3,8 @@
 rem This file is a part of SRC Repair project. For more information
 rem visit official site: https://www.easycoding.org/projects/srcrepair
 rem 
-rem Copyright (c) 2011 - 2019 EasyCoding Team (ECTeam).
-rem Copyright (c) 2005 - 2019 EasyCoding Team.
+rem Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+rem Copyright (c) 2005 - 2020 EasyCoding Team.
 rem 
 rem This program is free software: you can redistribute it and/or modify
 rem it under the terms of the GNU General Public License as published by
