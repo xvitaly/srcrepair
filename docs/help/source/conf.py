bfda7d48df509c348b23958121ed92bfec148894
Vitaly Zaitsev <vitaly@easycoding.org>
2020-02-17 17:59:40 +0100

    Enabled gettext support for Sphinx doc.

--- docs/help/source/conf.py
+++ docs/help/source/conf.py
@@ -56,6 +56,10 @@ extensions = ['sphinx.ext.todo']
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# Localization support.
+locale_dirs = ['locale']
+gettext_compact = False
+
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
