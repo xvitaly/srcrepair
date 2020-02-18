13a8c8b885e496bd1073cf5cbdd9f51221661ff2
Vitaly Zaitsev <vitaly@easycoding.org>
2020-02-18 10:29:39 +0100

    Added GPLv3+ preamble to Windows makefile.

--- docs/help/make.cmd
+++ docs/help/make.cmd
@@ -1,5 +1,24 @@
 @ECHO OFF
 
+rem This file is a part of SRC Repair project. For more information
+rem visit official site: https://www.easycoding.org/projects/srcrepair
+rem 
+rem Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+rem Copyright (c) 2005 - 2020 EasyCoding Team.
+rem 
+rem This program is free software: you can redistribute it and/or modify
+rem it under the terms of the GNU General Public License as published by
+rem the Free Software Foundation, either version 3 of the License, or
+rem (at your option) any later version.
+rem 
+rem This program is distributed in the hope that it will be useful,
+rem but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem GNU General Public License for more details.
+rem 
+rem You should have received a copy of the GNU General Public License
+rem along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 pushd %~dp0
 
 REM Command file for Sphinx documentation
