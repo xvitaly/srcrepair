5940f8c9caa1d9bfb49cf7ad7d03f830ad6e1ef6
Vitaly Zaitsev <vitaly@easycoding.org>
2020-03-09 10:48:17 +0100

    Added Type4Video class.

---
+++ corelib/Type4Video.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace srcrepair.core
+{
+    public class Type4Video : Type1Video
+    {
+        public Type4Video(string SAppName, bool ReadNow = true) : base (SAppName, false)
+        {
+            if (ReadNow)
+            {
+                ReadSettings();
+            }
+        }
+    }
+}
