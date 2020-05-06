22cfaa2ee89b58f86a305cf1ea64a049eabe0206
Vitaly Zaitsev <vitaly@easycoding.org>
2020-05-06 19:06:49 +0200

    Added missing GPLv3 preamble to Program.cs of KB Helper.
    Closes #359.

--- kbhelper/Program.cs
+++ kbhelper/Program.cs
@@ -1,9 +1,25 @@
-﻿using srcrepair.gui;
+﻿/*
+ * This file is a part of SRC Repair project. For more information
+ * visit official site: https://www.easycoding.org/projects/srcrepair
+ *
+ * Copyright (c) 2011 - 2020 EasyCoding Team (ECTeam).
+ * Copyright (c) 2005 - 2020 EasyCoding Team.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using srcrepair.core;
 
@@ -34,7 +50,7 @@ namespace srcrepair.gui.kbhelper
                 }
                 else
                 {
-                    MessageBox.Show(AppStrings.KB_AlrLaunched, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    MessageBox.Show(AppStrings.KB_AlrLaunched, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
                     Environment.Exit(ReturnCodes.AppAlreadyRunning);
                 }
             }
