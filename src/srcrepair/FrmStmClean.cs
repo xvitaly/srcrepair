ca4aa7701a30511cabfda3138456025be3ccf013
Vitaly Zaitsev <vitaly@easycoding.org>
2021-01-02 13:03:52 +0100

    Refactoring: extracted lots of methods from the FrmStmClean class.

--- src/srcrepair/FrmStmClean.cs
+++ src/srcrepair/FrmStmClean.cs
@@ -51,6 +51,11 @@ namespace srcrepair.gui
         private string SteamProcName { get; set; }
 
         /// <summary>
+        /// Gets or sets the list of files and directories for cleanup.
+        /// </summary>
+        private List<String> CleanDirs { get; set; }
+
+        /// <summary>
         /// FrmStmClean class constructor.
         /// </summary>
         /// <param name="SP">Full path to Steam client directory.</param>
@@ -64,122 +69,242 @@ namespace srcrepair.gui
             BackUpDir = BD;
             SteamAppsDirName = SA;
             SteamProcName = PN;
+            CleanDirs = new List<string>();
         }
 
         /// <summary>
-        /// "Execute cleanup" button click event handler.
+        /// Adds HTML cache of internal browser (both client and overlay) directories
+        /// to the list.
         /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void EC_Execute_Click(object sender, EventArgs e)
+        private void EnqueueHTMLCache()
         {
-            // Creatin an empty list for storing candidates...
-            List<String> CleanDirs = new List<string>();
-
-            // HTML cache of internal browser (both client and overlay)...
             if (EC_HTMLCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "config", "htmlcache", "*.*"));
                 CleanDirs.Add(Path.Combine(SteamPath, "config", "overlayhtmlcache", "*.*"));
                 CleanDirs.Add(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Steam", "htmlcache", "*.*"));
             }
+        }
 
-            // HTTP cache...
+        /// <summary>
+        /// Adds HTTP cache directories to the list.
+        /// </summary>
+        private void EnqueueHTTPCache()
+        {
             if (EC_HTTPCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "appcache", "httpcache", "*.*"));
             }
+        }
 
-            // Depot cache...
+        /// <summary>
+        /// Adds Depot cache directories to the list.
+        /// </summary>
+        private void EnqueueDepotCache()
+        {
             if (EC_DepotCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "depotcache", "*.*"));
             }
+        }
 
-            // Downloaded shaders cache...
+        /// <summary>
+        /// Adds Downloaded shaders cache directories to the list.
+        /// </summary>
+        private void EnqueueShaderCache()
+        {
             if (EC_ShaderCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, SteamAppsDirName, "shadercache", "*.*"));
             }
+        }
 
-            // Steam library cache...
+        /// <summary>
+        /// Adds Steam library cache directories to the list.
+        /// </summary>
+        private void EnqueueLibraryCache()
+        {
             if (EC_LibraryCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "appcache", "librarycache", "*.*"));
             }
+        }
 
-            // Client logs...
+        /// <summary>
+        /// Adds Client logs directories to the list.
+        /// </summary>
+        private void EnqueueClientLogs()
+        {
             if (EC_Logs.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "logs", "*.*"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.log*"));
             }
+        }
 
-            // Old binaries...
+        /// <summary>
+        /// Adds Old binaries directories to the list.
+        /// </summary>
+        private void EnqueueOldBinaries()
+        {
             if (EC_OldBins.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "*.old"));
                 CleanDirs.Add(Path.Combine(SteamPath, "bin", "*.old"));
             }
+        }
 
-            // Crash dumps...
+        /// <summary>
+        /// Adds Crash dumps directories to the list.
+        /// </summary>
+        private void EnqueueCrashDumps()
+        {
             if (EC_ErrDmps.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "dumps", "*.*"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.dmp"));
                 CleanDirs.Add(Path.Combine(SteamPath, "*.mdmp"));
             }
+        }
 
-            // Games download and build caches...
+        /// <summary>
+        /// Adds Games download and build caches directories to the list.
+        /// </summary>
+        private void EnqueueBuildCache()
+        {
             if (EC_BuildCache.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, SteamAppsDirName, "downloading"));
                 CleanDirs.Add(Path.Combine(SteamPath, SteamAppsDirName, "temp"));
             }
+        }
 
-            // Cached icons...
+        /// <summary>
+        /// Adds Cached icons directories to the list.
+        /// </summary>
+        private void EnqueueIconsCache()
+        {
             if (EC_GameIcons.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "steam", "games", "*.*"));
             }
+        }
 
-            // Local Cloud contents...
+        /// <summary>
+        /// Adds Local Cloud contents directories to the list.
+        /// </summary>
+        private void EnqueueCloudCache()
+        {
             if (EC_Cloud.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "userdata", "*.*"));
             }
+        }
 
-            // Local offline game statistics...
+        /// <summary>
+        /// Adds Local offline game statistics directories to the list.
+        /// </summary>
+        private void EnqueueStatsCache()
+        {
             if (EC_Stats.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "appcache", "stats", "*.*"));
             }
+        }
 
-            // Music database...
+        /// <summary>
+        /// Adds Music database directories to the list.
+        /// </summary>
+        private void EnqueueMusicDbCache()
+        {
             if (EC_Music.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "music", "_database", "*.*"));
             }
+        }
 
-            // Custom skins...
+        /// <summary>
+        /// Adds Custom skins directories to the list.
+        /// </summary>
+        private void EnqueueSkinsCache()
+        {
             if (EC_Skins.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "skins", "*.*"));
             }
+        }
 
-            // Old updater packages...
+        /// <summary>
+        /// Adds Old updater packages directories to the list.
+        /// </summary>
+        private void EnqueuePackagesCache()
+        {
             if (EC_Updater.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "package", "*.*"));
             }
+        }
 
-            // Guard supercookies and auth files...
+        /// <summary>
+        /// Adds Guard supercookies and auth files to the list.
+        /// </summary>
+        private void EnqueueGuardCache()
+        {
             if (EC_Guard.Checked)
             {
                 CleanDirs.Add(Path.Combine(SteamPath, "ssfn*"));
             }
+        }
 
-            // Executing cleanup sequence...
+        /// <summary>
+        /// Performs basic caches cleanup.
+        /// </summary>
+        private void CheckBasicCaches()
+        {
+            EnqueueHTMLCache();
+            EnqueueHTTPCache();
+            EnqueueDepotCache();
+            EnqueueShaderCache();
+            EnqueueLibraryCache();
+        }
+
+        /// <summary>
+        /// Performs common garbage cleanup.
+        /// </summary>
+        private void CheckCommonGarbage()
+        {
+            EnqueueClientLogs();
+            EnqueueOldBinaries();
+            EnqueueCrashDumps();
+            EnqueueBuildCache();
+        }
+
+        /// <summary>
+        /// Performs extended caches cleanup.
+        /// </summary>
+        private void CheckExtendedCleanup()
+        {
+            EnqueueIconsCache();
+            EnqueueCloudCache();
+            EnqueueStatsCache();
+            EnqueueMusicDbCache();
+            EnqueueSkinsCache();
+        }
+
+        /// <summary>
+        /// Performs special cleanup.
+        /// </summary>
+        private void CheckTroubleshooting()
+        {
+            EnqueuePackagesCache();
+            EnqueueGuardCache();
+        }
+
+        /// <summary>
+        /// Executes the cleanup sequence.
+        /// </summary>
+        private void PerformCleanup()
+        {
             if (CleanDirs.Count > 0)
             {
                 GuiHelpers.FormShowCleanup(CleanDirs, Text, AppStrings.PS_CleanupSuccess, BackUpDir, SteamProcName);
@@ -189,6 +314,20 @@ namespace srcrepair.gui
             {
                 MessageBox.Show(AppStrings.AC_NoItemsSelected, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
+        }
+
+        /// <summary>
+        /// "Execute cleanup" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void EC_Execute_Click(object sender, EventArgs e)
+        {
+            CheckBasicCaches();
+            CheckCommonGarbage();
+            CheckExtendedCleanup();
+            CheckTroubleshooting();
+            PerformCleanup();
         }
 
         /// <summary>
