b8800f8ef84e6718a9df164c041462aeb6637e8f
Vitaly Zaitsev <vitaly@easycoding.org>
2021-01-03 19:36:29 +0100

    Refactored CurrentPlatform class.
    Closes #403.

--- src/corelib/CurrentPlatform.cs
+++ src/corelib/CurrentPlatform.cs
@@ -19,15 +19,35 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 using System;
+using System.Diagnostics;
 using System.IO;
+using System.Security.Permissions;
 
 namespace srcrepair.core
 {
     /// <summary>
     /// Class for working with platform-dependent functions.
     /// </summary>
-    public sealed class CurrentPlatform
+    public abstract class CurrentPlatform
     {
+        /// <summary>
+        /// Create a platform-dependent instance. Factory method.
+        /// </summary>
+        public static CurrentPlatform Create()
+        {
+            switch (GetRunningOS())
+            {
+                case OSType.Windows:
+                    return new PlatformWindows();
+                case OSType.Linux:
+                    return new PlatformLinux();
+                case OSType.MacOSX:
+                    return new PlatformMac();
+                default:
+                    throw new PlatformNotSupportedException();
+            }
+        }
+
         /// <summary>
         /// Codes and IDs of available platforms.
         /// </summary>
@@ -39,104 +59,15 @@ namespace srcrepair.core
         }
 
         /// <summary>
-        /// Get current operating system ID.
-        /// </summary>
-        public OSType OS { get; private set; }
-
-        /// <summary>
         /// Get current operating system friendly name.
         /// </summary>
         public string OSFriendlyName => OS.ToString();
 
         /// <summary>
-        /// Get platform-dependent Steam launcher file name.
-        /// </summary>
-        public string SteamBinaryName
-        {
-            get
-            {
-                switch (OS)
-                {
-                    case OSType.Windows:
-                        return Properties.Resources.SteamExecBinWin;
-                    case OSType.Linux:
-                        return Properties.Resources.SteamExecBinLin;
-                    case OSType.MacOSX:
-                        return Properties.Resources.SteamExecBinMac;
-                    default:
-                        throw new PlatformNotSupportedException();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Get platform-dependent SteamApps directory name.
-        /// </summary>
-        public string SteamAppsFolderName
-        {
-            get
-            {
-                switch (OS)
-                {
-                    case OSType.Windows:
-                        return Properties.Resources.SteamAppsFolderNameWin;
-                    case OSType.Linux:
-                        return Properties.Resources.SteamAppsFolderNameLin;
-                    case OSType.MacOSX:
-                        return Properties.Resources.SteamAppsFolderNameMac;
-                    default:
-                        throw new PlatformNotSupportedException();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Get platform-dependent Steam process name.
-        /// </summary>
-        public string SteamProcName
-        {
-            get
-            {
-                switch (OS)
-                {
-                    case OSType.Windows:
-                        return Properties.Resources.SteamProcNameWin;
-                    case OSType.Linux:
-                        return Properties.Resources.SteamProcNameLin;
-                    case OSType.MacOSX:
-                        return Properties.Resources.SteamProcNameMac;
-                    default:
-                        throw new PlatformNotSupportedException();
-                }
-            }
-        }
-
-        /// <summary>
-        /// Get platform-dependent suffix for HTTP_USER_AGENT header.
-        /// </summary>
-        public string UASuffix
-        {
-            get
-            {
-                switch (OS)
-                {
-                    case OSType.Windows:
-                        return Properties.Resources.AppUASuffixWin;
-                    case OSType.Linux:
-                        return Properties.Resources.AppUASuffixOther;
-                    case OSType.MacOSX:
-                        return Properties.Resources.AppUASuffixOther;
-                    default:
-                        throw new PlatformNotSupportedException();
-                }
-            }
-        }
-
-        /// <summary>
         /// Get name and ID of running operating system.
         /// </summary>
         /// <returns>Platform ID.</returns>
-        private OSType GetRunningOS()
+        private static OSType GetRunningOS()
         {
             switch (Environment.OSVersion.Platform)
             {
@@ -149,12 +80,93 @@ namespace srcrepair.core
         }
 
         /// <summary>
-        /// CurrentPlatform class constructor.
+        /// Add quotes to the path.
         /// </summary>
-        public CurrentPlatform()
+        /// <param name="Source">Source string with path.</param>
+        /// <returns>Quoted string with path.</returns>
+        protected static string AddQuotesToPath(string Source)
         {
-            // Getting information about running operating system...
-            OS = GetRunningOS();
+            return String.Format(Properties.Resources.AppOpenHandlerEscapeTemplate, Source);
         }
+
+        /// <summary>
+        /// Open the specified URL in default Web browser.
+        /// </summary>
+        /// <param name="URI">Full URL.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public virtual void OpenWebPage(string URI)
+        {
+            Process.Start(URI);
+        }
+
+        /// <summary>
+        /// Restart current application with admin user rights.
+        /// </summary>
+        /// <param name="OS">Operating system type.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public virtual void RestartApplicationAsAdmin()
+        {
+            StartElevatedProcess(CurrentApp.AssemblyLocation);
+            Environment.Exit(ReturnCodes.Success);
+        }
+
+        /// <summary>
+        /// Get platform-dependent suffix for HTTP_USER_AGENT header.
+        /// </summary>
+        public virtual string UASuffix => Properties.Resources.AppUASuffixOther;
+
+        /// <summary>
+        /// Get current operating system ID.
+        /// </summary>
+        public abstract OSType OS { get; }
+
+        /// <summary>
+        /// Get platform-dependent Steam launcher file name.
+        /// </summary>
+        public abstract string SteamBinaryName { get; }
+
+        /// <summary>
+        /// Get platform-dependent SteamApps directory name.
+        /// </summary>
+        public abstract string SteamAppsFolderName { get; }
+
+        /// <summary>
+        /// Get platform-dependent Steam process name.
+        /// </summary>
+        public abstract string SteamProcName { get; }
+
+        /// <summary>
+        /// Return platform-dependent location of the Hosts file.
+        /// </summary>
+        public virtual string HostsFileLocation => "/etc";
+
+        /// <summary>
+        /// Return platform-dependent path to Hosts file.
+        /// </summary>
+        public virtual string HostsFileFullPath => Path.Combine(HostsFileLocation, "hosts");
+
+        /// <summary>
+        /// Open the specified text file in default (or overrided in application's
+        /// settings (only on Windows platform)) text editor.
+        /// </summary>
+        /// <param name="FileName">Full path to text file.</param>
+        /// <param name="EditorBin">External text editor (Windows only).</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public abstract void OpenTextEditor(string FileName, string EditorBin);
+
+        /// <summary>
+        /// Show the specified file in default file manager.
+        /// </summary>
+        /// <param name="FileName">Full path to file.</param>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public abstract void OpenExplorer(string FileName);
+
+        /// <summary>
+        /// Start the required application from administrator.
+        /// </summary>
+        /// <param name="FileName">Full path to the executable.</param>
+        /// <returns>PID of the newly created process.</returns>
+        [EnvironmentPermission(SecurityAction.Demand, Unrestricted = true)]
+        public abstract int StartElevatedProcess(string FileName);
     }
 }
