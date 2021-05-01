c2b7432032f846b22c9f9303ec1fcd8e57a38081
Vitaly Zaitsev <vitaly@easycoding.org>
2021-05-01 14:24:43 +0200

    Updated to version 42.0.1.7406.

--- src/kbhelper/Properties/AssemblyInfo.cs
+++ src/kbhelper/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("42.0.0.7400")]
-[assembly: AssemblyFileVersion("42.0.0.7400")]
+[assembly: AssemblyVersion("42.0.1.7406")]
+[assembly: AssemblyFileVersion("42.0.1.7406")]
