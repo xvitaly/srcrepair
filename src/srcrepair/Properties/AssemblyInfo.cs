f228d05e137954a4f467ec894d96051a56826baf
Vitaly Zaitsev <vitaly@easycoding.org>
2021-07-29 15:19:54 +0200

    Updated to version 43.0.0.

--- src/srcrepair/Properties/AssemblyInfo.cs
+++ src/srcrepair/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("42.0.1.7406")]
-[assembly: AssemblyFileVersion("42.0.1.7406")]
+[assembly: AssemblyVersion("43.0.0.7422")]
+[assembly: AssemblyFileVersion("43.0.0.7422")]
