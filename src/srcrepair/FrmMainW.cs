9cff1ab40687ae0f9fc9568fdd8d3a86524eeb1f
Vitaly Zaitsev <vitaly@easycoding.org>
2021-10-14 09:48:26 +0200

    Implemented GPG signatures verification.
    Closes #162.

--- src/srcrepair/FrmMainW.cs
+++ src/srcrepair/FrmMainW.cs
@@ -29,9 +29,11 @@ using System.Net;
 using System.Resources;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows.Forms;
 using NLog;
 using srcrepair.core;
+using srcrepair.gpg;
 
 namespace srcrepair.gui
 {
@@ -1746,6 +1748,37 @@ namespace srcrepair.gui
         #endregion
 
         /// <summary>
+        /// Asynchronically verifies the GPG signature of the specified file.
+        /// </summary>
+        /// <param name="FileName">Full path to the source file.</param>
+        /// <returns>Returns True if signature check passed.</returns>
+        private async Task<bool> VerifySignaturesTask(string FileName)
+        {
+            return await Task.Run(() =>
+            {
+                return Task.FromResult(GPGManager.VerifySignedFile(FileName));
+            });
+        }
+
+        /// <summary>
+        /// Verifies GPG signatures of the project.
+        /// </summary>
+        private async void VerifySignatures()
+        {
+            try
+            {
+                if (!await VerifySignaturesTask(CurrentApp.AssemblyLocation))
+                {
+                    Logger.Error("GPG signatures verifications failed.");
+                }
+            }
+            catch (Exception Ex)
+            {
+                Logger.Error(Ex);
+            }
+        }
+        
+        /// <summary>
         /// "Form create" event handler.
         /// </summary>
         /// <param name="sender">Sender object.</param>
@@ -1761,6 +1794,7 @@ namespace srcrepair.gui
             CheckSymbolsSteam();
             FindGames();
             CheckForUpdates();
+            VerifySignatures();
         }
 
         /// <summary>
