09e26a5e34a3807173e1d83b9cce252e9031b57e
Vitaly Zaitsev <vitaly@easycoding.org>
2021-10-18 10:44:35 +0200

    Converted all static constants to properties in ReturnCodes class.

--- src/corelib/ReturnCodes.cs
+++ src/corelib/ReturnCodes.cs
@@ -14,58 +14,58 @@ namespace srcrepair.core
         /// <summary>
         /// Successful exit without any errors.
         /// </summary>
-        public const int Success = 0;
+        public static int Success => 0;
 
         /// <summary>
         /// Wrong path to Steam client installation directory entered.
         /// </summary>
-        public const int StmWrongPath = 1;
+        public static int StmWrongPath => 1;
 
         /// <summary>
         /// User refused to find Steam client installation directory.
         /// </summary>
-        public const int StmPathCancel = 2;
+        public static int StmPathCancel => 2;
 
         /// <summary>
         /// Exception occured while trying to detect Steam client
         /// installation directory.
         /// </summary>
-        public const int StmPathException = 3;
+        public static int StmPathException => 3;
 
         /// <summary>
         /// No supported installed games detected.
         /// </summary>
-        public const int NoGamesDetected = 4;
+        public static int NoGamesDetected => 4;
 
         /// <summary>
         /// Exception during parsing XML game database and trying
         /// to find installed supported games.
         /// </summary>
-        public const int GameDbParseError = 5;
+        public static int GameDbParseError => 5;
 
         /// <summary>
         /// No Steam UserIDs detected.
         /// </summary>
-        public const int NoUserIdsDetected = 6;
+        public static int NoUserIdsDetected => 6;
 
         /// <summary>
         /// Application is already running.
         /// </summary>
-        public const int AppAlreadyRunning = 7;
+        public static int AppAlreadyRunning => 7;
 
         /// <summary>
         /// Core library version missmatch.
         /// </summary>
-        public const int CoreLibVersionMissmatch = 8;
+        public static int CoreLibVersionMissmatch => 8;
 
         /// <summary>
         /// Hosts file does not exists.
         /// </summary>
-        public const int HostsFileDoesNotExists = 9;
+        public static int HostsFileDoesNotExists => 9;
 
         /// <summary>
         /// Application update pending.
         /// </summary>
-        public const int AppUpdatePending = 10;
+        public static int AppUpdatePending => 10;
     }
 }
