596743da69d96dbf8ce1d77a6c08684b40a410a3
Vitaly Zaitsev <vitaly@easycoding.org>
2022-03-22 12:16:46 +0100

    Unified build scripts. Now can be used on CI too.

--- packaging/helpers/build_chm_win.cmd
+++ packaging/helpers/build_chm_win.cmd
@@ -10,12 +10,12 @@ title CHM builder for SRC Repair
 
 echo Building offline help for default (EN) locale...
 call "..\..\docs\make.cmd" htmlhelp
-"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" "..\..\docs\build\htmlhelp\srcrepair_en.hhp"
+"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" "..\..\docs\build\htmlhelp\srcrepair_en.hhp" & exit /b 1 | exit /b 0
 move "..\..\docs\build\htmlhelp\srcrepair_en.chm" "..\..\src\srcrepair\bin\Release\help\srcrepair_en.chm"
 
 echo Building offline help for RU locale...
 set SPHINXOPTS=-D language=ru
 set BUILDLANG=ru
 call "..\..\docs\make.cmd" htmlhelp
-"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" "..\..\docs\build\htmlhelp\srcrepair_ru.hhp"
+"%ProgramFiles(x86)%\HTML Help Workshop\hhc.exe" "..\..\docs\build\htmlhelp\srcrepair_ru.hhp" & exit /b 1 | exit /b 0
 move "..\..\docs\build\htmlhelp\srcrepair_ru.chm" "..\..\src\srcrepair\bin\Release\help\srcrepair_ru.chm"
