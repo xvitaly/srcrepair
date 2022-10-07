4af79ce12f01a045bd3f92bef8f2d64ef2160115
Vitaly Zaitsev <vitaly@easycoding.org>
2022-10-07 09:32:02 +0200

    Implemented custom exception type VideoConfigException.

---
+++ src/corelib/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿/**
+ * SPDX-FileCopyrightText: 2011-2022 EasyCoding Team
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+using System;
+
+namespace srcrepair.core
+{
+    /// <summary>
+    /// Class of custom VideoConfigException type.
+    /// </summary>
+    [Serializable]
+    public class VideoConfigException : Exception
+    {
+        public VideoConfigException() { }
+
+        public VideoConfigException(string Message) : base(Message) { }
+
+        public VideoConfigException(string Message, Exception InnerException) : base(Message, InnerException) { }
+    }
+}
