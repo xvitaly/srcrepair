88b387214330d964f85056a0eae2b6b8f24a8ed8
Vitaly Zaitsev <vitaly@easycoding.org>
2022-10-09 13:36:53 +0200

    Explicitly specified culture for string operations in AddBackUpsToTable().

--- src/srcrepair/FrmMainW.cs
+++ src/srcrepair/FrmMainW.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Drawing;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
@@ -1201,7 +1202,7 @@ namespace srcrepair.gui
                     {
                         Rs.Item1,
                         GuiHelpers.SclBytes(DItem.Length),
-                        DItem.CreationTime.ToString(),
+                        DItem.CreationTime.ToString(CultureInfo.CurrentUICulture),
                         DItem.Name
                     }
                 };
