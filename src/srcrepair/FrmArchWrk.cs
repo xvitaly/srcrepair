bfa3efc18456881ae4a44b29ecb7d1450b2fe05d
Vitaly Zaitsev <vitaly@easycoding.org>
2022-10-14 14:55:19 +0200

    Removed useless variable assignment in FrmArchWrk.

--- src/srcrepair/FrmArchWrk.cs
+++ src/srcrepair/FrmArchWrk.cs
@@ -66,7 +66,7 @@ namespace srcrepair.gui
                 {
                     // Creating some counters...
                     int TotalFiles = Zip.Count;
-                    int CurrentFile = 1, CurrentPercent = 0, PreviousPercent = 0;
+                    int CurrentFile = 1, CurrentPercent, PreviousPercent = 0;
 
                     // Unpacking archive contents...
                     foreach (ZipEntry ZFile in Zip)
