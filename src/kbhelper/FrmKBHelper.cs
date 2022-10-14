86be7e5789794717bcf12cf383964b80d9239dd0
Vitaly Zaitsev <vitaly@easycoding.org>
2022-10-14 15:15:12 +0200

    Reduced code duplication in FrmKBHelper.cs.

--- src/kbhelper/FrmKBHelper.cs
+++ src/kbhelper/FrmKBHelper.cs
@@ -58,43 +58,17 @@ namespace srcrepair.gui.kbhelper
         }
 
         /// <summary>
-        /// "Disable left WIN" button click event handler.
+        /// Installs specified keyboard settings override.
         /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void Dis_LWIN_Click(object sender, EventArgs e)
+        /// <param name="Value">Bytes array with new settings.</param>
+        /// <param name="Name">User-friendly setting name.</param>
+        private void InstallKBS(byte[] Value, string Name)
         {
-            // Disable left Windows button...
-            // 00 00 00 00 00 00 00 00 02 00 00 00 00 00 5B E0 00 00 00 00
-            if (MessageBox.Show(String.Format(AppStrings.KB_DisableQuestion, ((Button)sender).Text.ToLower()), Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if (MessageBox.Show(String.Format(AppStrings.KB_DisableQuestion, Name), Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
             {
                 try
                 {
-                    WriteKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 91, 224, 0, 0, 0, 0 });
-                    MessageBox.Show(AppStrings.KB_Success, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Close();
-                }
-                catch
-                {
-                    MessageBox.Show(AppStrings.KB_ExLeftWin, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-        }
-
-        /// <summary>
-        /// "Disable both WIN" button click event handler.
-        /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void Dis_BWIN_Click(object sender, EventArgs e)
-        {
-            // Disable both Windows keys...
-            // 00 00 00 00 00 00 00 00 03 00 00 00 00 00 5B E0 00 00 5C E0 00 00 00 00
-            if (MessageBox.Show(String.Format(AppStrings.KB_DisableQuestion, ((Button)sender).Text.ToLower()), Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                try
-                {
-                    WriteKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 91, 224, 0, 0, 92, 224, 0, 0, 0, 0 });
+                    WriteKBS(Value);
                     MessageBox.Show(AppStrings.KB_Success, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
                     Close();
                 }
@@ -106,62 +80,11 @@ namespace srcrepair.gui.kbhelper
         }
 
         /// <summary>
-        /// "Disable right WIN and CONTEXT" button click event handler.
+        /// Uninstalls all keyboard settings overrides.
         /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void Dis_RWinMnu_Click(object sender, EventArgs e)
+        private void UninstallKBS()
         {
-            // Disable right Windows and Context keys...
-            // 00 00 00 00 00 00 00 00 03 00 00 00 00 00 5C E0 00 00 5D E0 00 00 00 00
-            if (MessageBox.Show(String.Format(AppStrings.KB_DisableQuestion, ((Button)sender).Text.ToLower()), Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                try
-                {
-                    WriteKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 91, 224, 0, 0, 93, 224, 0, 0, 0, 0 });
-                    MessageBox.Show(AppStrings.KB_Success, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Close();
-                }
-                catch
-                {
-                    MessageBox.Show(AppStrings.KB_ExRWinMenu, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-        }
-
-        /// <summary>
-        /// "Disable both WIN and CONTEXT" button click event handler.
-        /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void Dis_BWinMnu_Click(object sender, EventArgs e)
-        {
-            // Disable both Windows and Context buttons...
-            // 00 00 00 00 00 00 00 00 04 00 00 00 00 00 5B E0 00 00 5C E0 00 00 5D E0 00 00 00 00
-            if (MessageBox.Show(String.Format(AppStrings.KB_DisableQuestion, ((Button)sender).Text.ToLower()), Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
-            {
-                try
-                {
-                    WriteKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 92, 224, 0, 0, 93, 224, 0, 0, 0, 0 });
-                    MessageBox.Show(AppStrings.KB_Success, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Information);
-                    Close();
-                }
-                catch
-                {
-                    MessageBox.Show(AppStrings.KB_ExBothWinMenu, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
-                }
-            }
-        }
-
-        /// <summary>
-        /// "Restore settings" button click event handler.
-        /// </summary>
-        /// <param name="sender">Sender object.</param>
-        /// <param name="e">Event arguments.</param>
-        private void Dis_Restore_Click(object sender, EventArgs e)
-        {
-            // Restoring default settings by removing manual overrides...
-            if (MessageBox.Show(AppStrings.KB_RestoreQuestion, Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes)
+            if (MessageBox.Show(AppStrings.KB_RestoreQuestion, Properties.Resources.AppName, MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1) == DialogResult.Yes)
             {
                 try
                 {
@@ -174,6 +97,65 @@ namespace srcrepair.gui.kbhelper
                     MessageBox.Show(AppStrings.KB_ExRestore, Properties.Resources.AppName, MessageBoxButtons.OK, MessageBoxIcon.Warning);
                 }
             }
+        }
+
+        /// <summary>
+        /// "Disable left WIN" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void Dis_LWIN_Click(object sender, EventArgs e)
+        {
+            // Disable left Windows button...
+            // 00 00 00 00 00 00 00 00 02 00 00 00 00 00 5B E0 00 00 00 00
+            InstallKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 91, 224, 0, 0, 0, 0 }, ((Button)sender).Text.ToLowerInvariant());
+        }
+
+        /// <summary>
+        /// "Disable both WIN" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void Dis_BWIN_Click(object sender, EventArgs e)
+        {
+            // Disable both Windows keys...
+            // 00 00 00 00 00 00 00 00 03 00 00 00 00 00 5B E0 00 00 5C E0 00 00 00 00
+            InstallKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 91, 224, 0, 0, 92, 224, 0, 0, 0, 0 }, ((Button)sender).Text.ToLowerInvariant());
+        }
+
+        /// <summary>
+        /// "Disable right WIN and CONTEXT" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void Dis_RWinMnu_Click(object sender, EventArgs e)
+        {
+            // Disable right Windows and Context keys...
+            // 00 00 00 00 00 00 00 00 03 00 00 00 00 00 5C E0 00 00 5D E0 00 00 00 00
+            InstallKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 91, 224, 0, 0, 93, 224, 0, 0, 0, 0 }, ((Button)sender).Text.ToLowerInvariant());
+        }
+
+        /// <summary>
+        /// "Disable both WIN and CONTEXT" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void Dis_BWinMnu_Click(object sender, EventArgs e)
+        {
+            // Disable both Windows and Context buttons...
+            // 00 00 00 00 00 00 00 00 04 00 00 00 00 00 5B E0 00 00 5C E0 00 00 5D E0 00 00 00 00
+            InstallKBS(new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 0, 0, 0, 0, 92, 224, 0, 0, 93, 224, 0, 0, 0, 0 }, ((Button)sender).Text.ToLowerInvariant());
+        }
+
+        /// <summary>
+        /// "Restore settings" button click event handler.
+        /// </summary>
+        /// <param name="sender">Sender object.</param>
+        /// <param name="e">Event arguments.</param>
+        private void Dis_Restore_Click(object sender, EventArgs e)
+        {
+            // Restoring default settings by removing manual overrides...
+            UninstallKBS();
         }
     }
 }
