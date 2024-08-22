487201f68172b68ef9ddfbb6c0e3e2407b4cff46
Vitaly <vitaly@easycoding.org>
2024-08-22 17:01:39 +0200

    Removed no longer needed workarounds for some types from the DpiManager class.

--- src/corelib/DpiManager.cs
+++ src/corelib/DpiManager.cs
@@ -16,20 +16,6 @@ namespace srcrepair.core
     public static class DpiManager
     {
         /// <summary>
-        /// Correctly scale columns width in DataGridView container on high
-        /// pixel density displays.
-        /// </summary>
-        /// <param name="ScaleSource">DataGridView control name.</param>
-        /// <param name="ScaleFactor">Scale factor value.</param>
-        public static void ScaleColumnsInControl(DataGridView ScaleSource, SizeF ScaleFactor)
-        {
-            foreach (DataGridViewColumn Column in ScaleSource.Columns)
-            {
-                Column.Width = (int)Math.Round(Column.Width * ScaleFactor.Width);
-            }
-        }
-
-        /// <summary>
         /// Correctly scale columns width in ListView container on high
         /// pixel density displays.
         /// </summary>
