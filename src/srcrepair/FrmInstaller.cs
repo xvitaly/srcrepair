573b202c916fde7fc37cabed3f2124ab998d8e98
Vitaly <vitaly@easycoding.org>
2024-11-24 10:14:26 +0100

    Added more debug info to the FrmInstaller class.

--- src/srcrepair/FrmInstaller.cs
+++ src/srcrepair/FrmInstaller.cs
@@ -79,7 +79,7 @@ namespace srcrepair.gui
                 }
                 catch (Exception Ex)
                 {
-                    Logger.Warn(Ex);
+                    Logger.Warn(Ex, DebugStrings.AppDbgExInstallerCompile);
                 }
             }
         }
