7d46bddbe89f16d25bad8c1c4aafce44cd5539d2
Vitaly <vitaly@easycoding.org>
2025-01-27 14:24:05 +0100

    Added a new empty class CleanupItem.

---
+++ src/corelib/CleanupItem.cs
@@ -0,0 +1,14 @@
+﻿/**
+ * SPDX-FileCopyrightText: 2011-2025 EasyCoding Team
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
+using System;
+
+namespace srcrepair.core
+{
+    public sealed class CleanupItem
+    {
+    }
+}
